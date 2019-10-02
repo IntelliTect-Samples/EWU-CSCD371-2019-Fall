0cbfc0146b109d780e88ee3010d0ac574d50d0e2
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-10-02 09:07:28 -0700

    Randomize questions every run

--- PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
+++ PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace PrincessBrideTrivia
 {
@@ -8,7 +9,7 @@ namespace PrincessBrideTrivia
         public static void Main(string[] args)
         {
             string filePath = GetFilePath();
-            Question[] questions = LoadQuestions(filePath);
+            Question[] questions = RandomizeQuestions(LoadQuestions(filePath));
 
             int numberCorrect = 0;
             for (int i = 0; i < questions.Length; i++)
@@ -92,6 +93,12 @@ namespace PrincessBrideTrivia
                 questions[i] = question;
             }
             return questions;
+        }
+
+        public static Question[] RandomizeQuestions(Question[] questions)
+        {
+            Random rnd = new Random();
+            return questions.OrderBy(question => rnd.Next()).ToArray();
         }
     }
 }
