40efd3932fd428b56c32b424e342472aaceec3a6
Asher Mancinelli <ashermancinelli@gmail.com>
2019-10-08 14:40:16 -0700

    added main method

--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -2,6 +2,7 @@
 {
     public abstract class BaseLogger
     {
+        private string _LoggerName;
         public abstract string LoggerName { get; set; }
         public abstract void Log(LogLevel logLevel, string message);
     }
