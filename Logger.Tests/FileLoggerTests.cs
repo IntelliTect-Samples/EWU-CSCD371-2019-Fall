e9a011d3a6cad7e3f4001aaf2132628b54690d2f
Asher Mancinelli <ashermancinelli@gmail.com>
2019-10-08 19:39:43 -0700

    better FileLogger test

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -8,11 +8,28 @@ namespace Logger.Tests
     public class FileLoggerTests
     {
         [TestMethod]
-        public void LogMessage_DidCreateFile() {
+        public void LogMessage_DidCreateFile_Success() {
             string fileName = "TestFile";
             FileLogger fileLogger = new FileLogger(fileName) { LoggerName = "TestLogger" };
             fileLogger.Log(LogLevel.Warning, "Test Message");
             Assert.IsTrue(File.Exists($"{fileName}.log"));
+            File.Delete($"{fileName}.log");
+        }
+
+        [TestMethod]
+        public void LogMessage_DidFormatLogMessage_Correct()
+        {
+            string fileName = "TestFile";
+            FileLogger fileLogger = new FileLogger(fileName) { LoggerName = "TestLogger" };
+            fileLogger.Log(LogLevel.Warning, "Test Message");
+            fileLogger.Log(LogLevel.Error, "Test Message");
+            fileLogger.Log(LogLevel.Information, "Test Message");
+            fileLogger.Log(LogLevel.Debug, "Test Message");
+            foreach (line in File.ReadLines($"{fileName}.log")) {
+                string[] words = line.Split(" ");
+                Assert.
+            }
+
             File.Delete($"{fileName}.log");
         }
     }
