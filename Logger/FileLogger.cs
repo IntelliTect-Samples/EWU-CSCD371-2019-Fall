a5e121d871c190aa8dd4b07cc765346811c3b6cc
Andrew Sales <dnisyndrum@yahoo.com>
2019-10-09 15:55:06 -0700

    Mild progress. Created FileLogger and implemented functions in FileLogger and LogFactory.

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Logger
+{
+    public class FileLogger : BaseLogger
+    {
+        StreamWriter sw;
+
+        public FileLogger(string filePath) 
+        {
+            if(!File.Exists(filePath))
+            {
+                sw = File.CreateText(filePath);
+            }   
+        }
+
+        public override void Log(LogLevel logLevel, string message)
+        {
+            DateTime now = DateTime.Now;
+            sw.WriteLine(now);
+            sw.WriteLine(className);
+            sw.WriteLine(logLevel.ToString() + ":");
+            sw.WriteLine(message);
+        }
+    }
+}
--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -2,10 +2,18 @@
 {
     public class LogFactory
     {
+        private string path;
         public BaseLogger CreateLogger(string className)
         {
-            
-            return null;
+            BaseLogger newBaseLogger = new FileLogger(this.path);
+            newBaseLogger.className = className;
+
+            return newBaseLogger;
+        }
+
+        public void ConfigureFileLogger(string newPath)
+        {
+
         }
     }
 }
