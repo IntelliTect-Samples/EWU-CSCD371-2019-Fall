b24ece125b638b27f2bde96df2caea185c947c07
TatyanaHubbard <tatsihubbard@gmail.com>
2019-10-11 11:56:05 -0700

    Began Assignment 2

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Logger
+{
+    public class FileLogger : BaseLogger
+    {
+        private string filePath;
+
+        public void SetFilePath(string newFilePath)
+        {
+            filePath = newFilePath;
+
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath);
+            }
+        }
+
+        public override void Log(LogLevel logLevel, string message)
+        {
+            string messageLog = $"{DateTime.Now} {ClassName} {logLevel}: {message}";
+
+            using (StreamWriter streamWriter = new StreamWriter(filePath))
+            {
+                streamWriter.WriteLine(messageLog);
+            }
+        }
+    }
+}
--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -2,10 +2,28 @@
 {
     public class LogFactory
     {
+        private string filePath;
         public BaseLogger CreateLogger(string className)
         {
-            
-            return null;
+            if(filePath == null)
+            {
+                return null;
+            }
+            else
+            {
+                FileLogger fileLogger = new FileLogger
+                {
+                    ClassName = className
+                };
+
+                fileLogger.SetFilePath(filePath);
+                return fileLogger;
+            }
+        }
+
+        public void ConfigureFileLogger(string newFilePath)
+        {
+            filePath = newFilePath;
         }
     }
 }
