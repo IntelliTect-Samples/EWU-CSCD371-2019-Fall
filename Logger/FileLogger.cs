f70ff42f32aa8b17603126186eadbf0a0663f846
Josh Lini <networkmisc@gmail.com>
2019-10-10 21:21:34 -0700

    added auto-prop to BaseLogger and created skeleton of FileLogger

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,14 @@
+﻿using Logger;
+using System;
+
+public class FileLogger : BaseLogger
+{
+	public FileLogger()
+	{
+	}
+
+    public override void Log(LogLevel logLevel, string message)
+    {
+        throw new NotImplementedException();
+    }
+}
