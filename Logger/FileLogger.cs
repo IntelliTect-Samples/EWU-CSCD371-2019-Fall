b1fff0d8074db0702c4541a8d1a6bb41117b7ccc
Josh Lini <networkmisc@gmail.com>
2019-10-10 22:02:14 -0700

    Starting on FileLogger.Log()

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -1,16 +1,28 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace Logger
 {
     public class FileLogger : BaseLogger
     {
-        public FileLogger()
+        private String path;
+        
+        public FileLogger(String path)
         {
+            this.path = path;
         }
 
         public override void Log(LogLevel logLevel, string message)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            sb.Append(DateTime.Now);
+            sb.Append(ClassName);
+            sb.Append(logLevel);
+            sb.Append(message);
+            sb.Append(Environment.NewLine);
+
+            File.AppendAllText(path, sb.ToString());
         }
     }
 }
