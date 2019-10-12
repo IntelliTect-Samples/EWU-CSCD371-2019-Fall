b08885bcf9925d1016710427ac09d385e44ebee9
Josh Lini <networkmisc@gmail.com>
2019-10-11 19:13:06 -0700

    Added Error() extension method

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,7 +1,19 @@
-﻿namespace Logger
+﻿using System;
+
+namespace Logger
 {
     public static class BaseLoggerMixins
     {
-        
+        public static void Error(this BaseLogger baseLogger, string message, params int[] v)
+        {
+            string[] parameters = new string[v.Length];
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                parameters[i] = v[i].ToString();
+            }
+
+            baseLogger.Log(LogLevel.Error, String.Format(message, parameters));//check for null?
+        }
     }
 }
