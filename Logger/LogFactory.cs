17d8d08b4dce564b4a943568cd405a73dcc4741b
CDobbins1 <cdobsc01@hotmail.com>
2019-10-13 12:22:57 -0700

    Assignment2 small update

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -5,16 +5,9 @@
         private string filePath;
         public BaseLogger CreateLogger(string className)
         {
-            if (filePath is null)
-            {
-                return null;
-            }
+            if (filePath is null) return null;
 
-            var logger = new FileLogger(filePath) { ClassName = className };
-            if (logger.ClassName is null)
-            {
-                logger.ClassName = "CLASSNAME NOT GIVEN";
-            }
+            var logger = new FileLogger(filePath) { ClassName = className ?? "CLASSNAME NOT GIVEN" };
             return logger;
         }
 
