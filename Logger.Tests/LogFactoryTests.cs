508e0795b0713511238f76eabc0e10b7ea838568
Joel <joelkalich1111@gmail.com>
2019-10-13 16:40:13 -0700

    Added unit tests for LogFactory

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
 
 namespace Logger.Tests
@@ -6,6 +7,31 @@ namespace Logger.Tests
     [TestClass]
     public class LogFactoryTests
     {
+        [TestMethod]
+        public void CreateLogger_ReturnsNullIfoutPathIsNull()
+        {
+            //arrange
+
+            //act
+            LogFactory logFactory = new LogFactory();
+
+            //assert
+            Assert.IsNull(logFactory.CreateLogger("className"));
+        }
         
+        [TestMethod]
+        public void CreateLogger_CreatesLogger()
+        {
+            //arrange
+            string userName = Environment.UserName;
+            string path = "C:/Users/" + userName + "/Documents/Visual Studio Logs/logs3.txt";
+
+            //act
+            LogFactory logFactory = new LogFactory();
+            logFactory.ConfigureFileLogger(path);
+
+            //assert
+            Assert.IsNotNull(logFactory.CreateLogger("className"));
+        }
     }
 }
