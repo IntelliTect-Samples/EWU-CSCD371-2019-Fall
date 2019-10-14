912b568080b309ab8023ddda05ce92f75af3bd60
Josh Lini <networkmisc@gmail.com>
2019-10-14 16:59:17 -0700

    updated Logger to Core3.0 due to an error. Started on more extensions

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -4,6 +4,12 @@ namespace Logger
 {
     public static class BaseLoggerMixins
     {
+        private static string LogLevelMessage(LogLevel LL, string message)
+        {
+
+            return null;//TODO
+        }
+
         public static void Error(this BaseLogger baseLogger, string message, params int[] v)
         {
             if (baseLogger is null) { throw new ArgumentNullException(); }
@@ -15,7 +21,7 @@ namespace Logger
                 parameters[i] = v[i].ToString();
             }
 
-            baseLogger.Log(LogLevel.Error, String.Format(message, parameters));//check for null?
+            baseLogger.Log(LogLevel.Error, string.Format(message, parameters));//check for null?
         }
     }
 }
