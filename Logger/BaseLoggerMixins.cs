04a3b91d5bdb96fdd9807b2983d55378e3216efb
Fervidusletum <fervidusletum@gmail.com>
2019-10-13 17:52:21 -0700

    Part 5

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,7 +1,26 @@
-﻿namespace Logger
+﻿using System;
+
+namespace Logger
 {
     public static class BaseLoggerMixins
     {
-        
+        public static void Error(this BaseLogger logger, string message, params object[] args)
+            => WriteLog(logger, LogLevel.Error, message, args);
+
+        public static void Warning(this BaseLogger logger, string message, params object[] args)
+            => WriteLog(logger, LogLevel.Warning, message, args);
+
+        public static void Information(this BaseLogger logger, string message, params object[] args)
+            => WriteLog(logger, LogLevel.Information, message, args);
+
+        public static void Debug(this BaseLogger logger, string message, params object[] args)
+            => WriteLog(logger, LogLevel.Debug, message, args);
+
+        private static void WriteLog(BaseLogger logger, LogLevel level, string message, params object[] args)
+        {
+            if (logger == null) throw new ArgumentNullException(nameof(logger));
+
+            logger.Log(level, string.Format(message, args));
+        }
     }
 }
