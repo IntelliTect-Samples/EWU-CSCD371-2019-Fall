c114799b529c3ef09bd89d5894688639dedb3cf6
dbosco051394 <abosco@eagles.ewu.edu>
2019-10-14 12:11:50 -0700

    Successfully implemented and unit tested the last issue. Assignment 2 is complete. Extra credit was not attempted.

--- Logger.Tests/BaseLoggerMixinsTests.cs
+++ Logger.Tests/BaseLoggerMixinsTests.cs
@@ -14,7 +14,7 @@ namespace Logger.Tests
             // Arrange
 
             // Act
-            //BaseLoggerMixins.Error(null, "");
+            BaseLoggerMixins.Error(null, "");
 
             // Assert
         }
@@ -26,12 +26,41 @@ namespace Logger.Tests
             var logger = new TestLogger();
 
             // Act
-            //logger.Error("Message {0}", 42);
+            logger.Error("Message {0}", 42);
 
             // Assert
             Assert.AreEqual(1, logger.LoggedMessages.Count);
             Assert.AreEqual(LogLevel.Error, logger.LoggedMessages[0].LogLevel);
             Assert.AreEqual("Message 42", logger.LoggedMessages[0].Message);
+        }
+
+        [TestMethod]
+        public void Success_AllExtensionMethods()
+        {
+            //Arrange
+            var errorLogger = new TestLogger();
+            var warningLogger = new TestLogger();
+            var informationLogger = new TestLogger();
+            var debugLogger = new TestLogger();
+
+            //Act
+            errorLogger.Error("Message {0}", 1);
+            warningLogger.Warning("Message {0}", 2);
+            informationLogger.Information("Message {0}", 3);
+            debugLogger.Debug("Message {0}", 4);
+
+            //Assert
+            Assert.AreEqual(LogLevel.Error, errorLogger.LoggedMessages[0].LogLevel);
+            Assert.AreEqual("Message 1", errorLogger.LoggedMessages[0].Message);
+
+            Assert.AreEqual(LogLevel.Warning, warningLogger.LoggedMessages[0].LogLevel);
+            Assert.AreEqual("Message 2", warningLogger.LoggedMessages[0].Message);
+
+            Assert.AreEqual(LogLevel.Information, informationLogger.LoggedMessages[0].LogLevel);
+            Assert.AreEqual("Message 3", informationLogger.LoggedMessages[0].Message);
+
+            Assert.AreEqual(LogLevel.Debug, debugLogger.LoggedMessages[0].LogLevel);
+            Assert.AreEqual("Message 4", debugLogger.LoggedMessages[0].Message);
         }
 
     }
--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,7 +1,28 @@
-﻿namespace Logger
+﻿using System;
+
+namespace Logger
 {
     public static class BaseLoggerMixins
     {
-        
+        public static void Error(this BaseLogger baseLogger, string message, params object[] messageArgs)
+        {
+            if (baseLogger == null) throw new ArgumentNullException("BaseLogger is null");
+            baseLogger.Log(LogLevel.Error, string.Format(message, messageArgs));
+        }
+        public static void Warning(this BaseLogger baseLogger, string message, params object[] messageArgs)
+        {
+            if (baseLogger == null) throw new ArgumentNullException("BaseLogger is null");
+            baseLogger.Log(LogLevel.Warning, string.Format(message, messageArgs));
+        }
+        public static void Information(this BaseLogger baseLogger, string message, params object[] messageArgs)
+        {
+            if (baseLogger == null) throw new ArgumentNullException("BaseLogger is null");
+            baseLogger.Log(LogLevel.Information, string.Format(message, messageArgs));
+        }
+        public static void Debug(this BaseLogger baseLogger, string message, params object[] messageArgs)
+        {
+            if (baseLogger == null) throw new ArgumentNullException("BaseLogger is null");
+            baseLogger.Log(LogLevel.Debug, string.Format(message, messageArgs));
+        }
     }
 }
