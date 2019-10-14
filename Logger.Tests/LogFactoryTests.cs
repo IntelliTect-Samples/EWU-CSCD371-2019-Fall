74a0768549326e663ff7a4d83000af284bf256b0
Josh Lini <networkmisc@gmail.com>
2019-10-14 16:40:43 -0700

    Added Unit Tests for FileLogger and LogFactory

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -6,6 +6,33 @@ namespace Logger.Tests
     [TestClass]
     public class LogFactoryTests
     {
-        
+        [TestMethod]
+        public void CreateLogger_BeforeConfiguring_ReturnsNull()
+        {
+            //Arrange
+            LogFactory lF = new LogFactory();
+            BaseLogger bL =  new FileLogger("placeholder.txt"); //Starting with non-null to ensure the action is what made bL null
+
+            //Act
+            bL = lF.CreateLogger("LogFactoryTests");
+
+            //Assert
+            Assert.IsTrue(bL is null);
+        }
+
+        [TestMethod]
+        public void CreateLogger_AfterConfiguring_ReturnsBaseLogger()
+        {
+            //Arrange
+            LogFactory lF = new LogFactory();
+            BaseLogger bL;
+
+            //Act
+            lF.ConfigureFileLogger("placeholder.txt");
+            bL = lF.CreateLogger("LogFactoryTests");
+
+            //Assert
+            Assert.IsNotNull(bL);
+        }
     }
 }
