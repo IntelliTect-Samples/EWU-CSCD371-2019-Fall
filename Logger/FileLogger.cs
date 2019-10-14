9822c652f1760858c20b6642c2dca17d6d941368
CDobbins1 <cdobsc01@hotmail.com>
2019-10-14 10:15:59 -0700

    Small change to FileLogger

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -6,9 +6,9 @@
         public FileLogger(string filePath) => this.filePath = filePath;
         public override void Log(LogLevel logLevel, string message)
         {
-            var sb = new System.Text.StringBuilder($"{System.DateTime.Now.ToString()} {ClassName} {logLevel}: {message}\n");
+            string logMessage = $"{System.DateTime.Now.ToString()} {ClassName} {logLevel}: {message}\n";
 
-            System.IO.File.AppendAllText(filePath, sb.ToString());
+            System.IO.File.AppendAllText(filePath, logMessage);
         }
     }
 }
