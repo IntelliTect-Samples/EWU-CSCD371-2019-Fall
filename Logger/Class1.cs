f9fbf2d57a7e6ab484210730d2dfb7bbdba6273b
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-10-14 22:08:04 -0700

    Add FileLogger class

---
+++ Logger/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Logger
+{
+    class FileLogger
+    {
+        public string Path { get; }
+
+        public FileLogger(string filePath)
+        {
+            Path = filePath ?? throw new ArgumentNullException(nameof(filePath));
+        }
+    }
+}
