1b4c53d17452612b9256325d9830aa5b677e1127
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-10-14 22:43:29 -0700

    Improve FileLogger tests

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -11,17 +11,25 @@ namespace Logger.Tests
         {
             string logFile = "FileLogger_Log_MessageIsCorrect.log";
             string testMessage = "abcdefghijklmnopqrstuvwxyz";
-            FileLogger logger = new FileLogger(logFile)
+
+            try
             {
-                ClassName = nameof(FileLoggerTests)
-            };
+                FileLogger logger = new FileLogger(logFile)
+                {
+                    ClassName = nameof(FileLoggerTests)
+                };
 
-            logger.Log(LogLevel.Error, testMessage);
+                logger.Log(LogLevel.Error, testMessage);
 
-            string[] logResults = File.ReadAllLines(logFile);
+                string[] logResults = File.ReadAllLines(logFile);
 
-            Assert.IsTrue(logResults.Length == 1);
-            Assert.IsTrue(logResults[0].Contains(testMessage));
+                Assert.AreEqual(1, logResults.Length);
+                Assert.IsTrue(logResults[0].Contains(testMessage));
+            }
+            finally
+            {
+                File.Delete(logFile);
+            }
         }
     }
 }
