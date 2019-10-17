ba53261a2f1ccbe3252f7401431f772e1b8ee5a3
Jake <jmcgillicuddy@eagles.ewu.edu>
2019-10-17 12:17:43 -0700

    Added classes and implementations

---
+++ Inheritance/src/Inheritance/ActorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public static class ActorExtensions
+    {
+        public static void Speak(this Actor actor)
+        {
+            
+        }
+    }
+}
