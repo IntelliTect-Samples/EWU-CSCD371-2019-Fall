d8cb8587f61c89e74b509410d24d1751789af2ac
Jacob Berger <jberger.1919@gmail.com>
2019-10-17 08:50:33 -0700

    Assignment 3 Part 1
    Created Television, implemented PrintInfo for Food and Television.

---
+++ Inheritance/src/Inheritance/Television.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Television : Item
+    {
+        public string Size { get; set; }
+        public string Manufacturer { get; set; }
+
+        public override string PrintInfo()
+        {
+            return $"{Manufacturer} - {Size}";
+        }
+    }
+}
