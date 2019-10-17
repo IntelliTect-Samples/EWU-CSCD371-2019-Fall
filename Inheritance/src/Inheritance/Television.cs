1d1a26b7824c23e9e864dbee7885a666f4126198
Khanh <kluu@eagles.ewu.edu>
2019-10-17 10:14:59 -0700

    Finished P1

---
+++ Inheritance/src/Inheritance/Television.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Television:Item
+    {
+        private string Manufacturor { get; set; }
+
+        private string Size { get; set; }
+
+        public override PrintInfo() => $"<{Manufacturor}> - <{Size}>";
+    }
+}
