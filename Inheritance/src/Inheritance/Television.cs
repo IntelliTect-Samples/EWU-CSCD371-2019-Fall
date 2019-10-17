00a49a29acd7332c130e4d551a1e3924525dde06
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-10-17 07:36:55 -0700

    Add Television class and tests

---
+++ Inheritance/src/Inheritance/Television.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Inheritance
+{
+    public class Television : Item
+    {
+        string Size { get; }
+        string Manufacturer { get; }
+
+        public Television(string size, string manufacturer)
+        {
+            if (size is null) throw new ArgumentNullException(nameof(Size));
+            if (manufacturer is null) throw new ArgumentNullException(nameof(manufacturer));
+            Size = size;
+            Manufacturer = manufacturer;
+        }
+
+        public override string PrintInfo() => $"<{Manufacturer}> - <{Size}>";
+    }
+}
