67648cff81ba9492cf9a55c616165e0ca319c4ce
dbosco051394 <abosco@eagles.ewu.edu>
2019-10-19 22:58:59 -0700

    Part 2 is implemented and fully tested. Ready for code review.

---
+++ Inheritance/src/Inheritance/Situation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public static class Situation
+    {
+        public static bool WomenArePresent = false;
+        
+        public static string Speak(this Actor a)
+        {
+            string type = a.GetType().Name;
+            if (type == "Penny")
+            {
+                WomenArePresent = true;
+                return "Hello, I am Penny.";
+            }
+            else if (type == "Sheldon")
+            {
+                return "Hello, I am Sheldon.";
+            }
+            else if (type == "Raj")
+            {
+                return WomenArePresent ? "mumble..." : "Hello, I am Raj."; 
+            }
+            else
+            {
+                return "bad input";
+            }
+        }
+    }
+}
--- Inheritance/test/Inheritance.Tests/ActorTests.cs
+++ Inheritance/test/Inheritance.Tests/ActorTests.cs
@@ -6,8 +6,95 @@ namespace Inheritance.Tests
     public class ActorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SheldonSpeaksAlone()
         {
+            //Arrange
+            Actor sheldon = new Sheldon();
+            string spokenWords = "";
+            string expectedWords = "Hello, I am Sheldon.";
+
+            //Act
+            spokenWords = sheldon.Speak();
+
+            //Assert
+            Assert.AreEqual(spokenWords, expectedWords);
+            Assert.IsFalse(Situation.WomenArePresent);
+        }
+
+        [TestMethod]
+        public void RajSpeaksAlone()
+        {
+            //Arrange
+            Actor raj = new Raj();
+            string spokenWords = "";
+            string expectedWords = "Hello, I am Raj.";
+
+            //Act
+            spokenWords = raj.Speak();
+
+            //Assert
+            Assert.AreEqual(spokenWords, expectedWords);
+            Assert.IsFalse(Situation.WomenArePresent);
+        }
+
+        [TestMethod]
+        public void PennySpeaksAlone()
+        {
+            //Arrange
+            Actor penny = new Penny();
+            string spokenWords = "";
+            string expectedWords = "Hello, I am Penny.";
+
+            //Act
+            spokenWords = penny.Speak();
+
+            //Assert
+            Assert.IsTrue(Situation.WomenArePresent);
+            Assert.AreEqual(spokenWords, expectedWords);
+        }
+
+        [TestMethod]
+        public void RajAndSheldonSpeak()
+        {
+            //Arrange
+            Situation.WomenArePresent = false;
+            Actor sheldon = new Sheldon();
+            Actor raj = new Raj();
+            string spokenWordsSheldon = "";
+            string expectedWordsSheldon = "Hello, I am Sheldon.";
+            string spokenWordsRaj = "";
+            string expectedWordsRaj = "Hello, I am Raj.";
+
+            //Act
+            spokenWordsSheldon = sheldon.Speak();
+            spokenWordsRaj = raj.Speak();
+
+            //Assert
+            Assert.AreEqual(spokenWordsSheldon, expectedWordsSheldon);
+            Assert.AreEqual(spokenWordsRaj, expectedWordsRaj);
+            Assert.IsFalse(Situation.WomenArePresent);
+        }
+
+        [TestMethod]
+        public void RajAndPennySpeak()
+        {
+            //Arrange
+            Situation.WomenArePresent = false;
+            Actor penny = new Penny();
+            Actor raj = new Raj();
+            string spokenWordsPenny = "";
+            string expectedWordsPenny = "Hello, I am Penny.";
+            string spokenWordsRaj = "";
+            string expectedWordsRaj = "mumble...";
+
+            //Act
+            spokenWordsPenny = penny.Speak();
+            spokenWordsRaj = raj.Speak();
+
+            //Assert
+            Assert.IsTrue(Situation.WomenArePresent);
+            Assert.AreEqual(spokenWordsPenny, expectedWordsPenny);
+            Assert.AreEqual(spokenWordsRaj, expectedWordsRaj);
         }
     }
 }
