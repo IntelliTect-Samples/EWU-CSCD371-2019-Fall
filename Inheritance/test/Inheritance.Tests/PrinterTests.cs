370d9799784624de3bf1841fdc117128a8de3649
dbosco051394 <abosco@eagles.ewu.edu>
2019-10-19 20:35:16 -0700

    Part 1 implemented and tested. Ready for code review.

--- Inheritance/test/Inheritance.Tests/PrinterTests.cs
+++ Inheritance/test/Inheritance.Tests/PrinterTests.cs
@@ -34,9 +34,66 @@ namespace Inheritance.Tests
                 }
             }
         }
+
+        [TestMethod]
+        public void Food_PrintInfo()
+        {
+            //Arrange
+            var foodItem = new Food { Brand = "McDonalds", Upc = "1004" };
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    //Act
+                    Printer.Print(foodItem, writer);
+                    writer.Flush();
+
+                    stream.Position = 0;
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    //Assert
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var lineWritten = reader.ReadLine();
+                        Assert.AreEqual(foodItem.PrintInfo(), lineWritten);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Television_PrintInfo()
+        {
+            //Arrange
+            var tvItem = new Television { Manufacturer="Vizio", Size="15" };
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    //Act
+                    Printer.Print(tvItem, writer);
+                    writer.Flush();
+
+                    stream.Position = 0;
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    //Assert
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var lineWritten = reader.ReadLine();
+                        Assert.AreEqual(tvItem.PrintInfo(), lineWritten);
+                    }
+                }
+            }
+        }
     }
 
     public class TestItem : Item {
         public string Name { get; set; }
+
+        public override string PrintInfo()
+        {
+            return Name;
+        }
     }
 }
