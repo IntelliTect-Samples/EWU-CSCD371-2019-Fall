023215a249925e2e9b898002c545139661523872
Doug Doner <ddoner02@gmail.com>
2019-10-22 10:19:27 -0700

    Adds Tests and files for Actor, ActorMixins, and Actor subclasses

---
+++ Inheritance/src/Inheritance/ActorMixins.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public static class ActorMixins
+    {
+        public static String Speak(this Actor actor) =>
+        actor switch
+        {
+        Penny p => "Hey I'm Penny",
+        Sheldon s => "Bapzingus",
+        Raj { WomenArePresent: false} r => "Hello friends",
+        Raj { WomenArePresent: true} r => "mumble",
+        { } => throw new ArgumentException(),
+        _ => throw new ArgumentNullException()
+        };
+    }
+}
---
+++ Inheritance/test/Inheritance.Tests/ActorMixinsTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Inheritance.Tests
+{
+    [TestClass]
+    public class ActorMixinsTests
+    {
+
+        [TestMethod]
+        public void ActorMixins_Penny_Speaks()
+        {
+            //Arrange
+            Actor penny = new Penny();
+
+            //Act
+
+            //Assert
+            Assert.AreEqual("Hey I'm Penny", penny.Speak());
+        }
+
+        [TestMethod]
+        public void ActorMixins_Sheldon_Speaks()
+        {
+            //Arrange
+            Actor sheldon = new Sheldon();
+
+            //Act
+
+            //Assert
+            Assert.AreEqual("Bapzingus", sheldon.Speak());
+        }
+
+        [TestMethod]
+        public void ActorMixins_Raj_NoWomenPresent_Speaks()
+        {
+            //Arrange
+            Actor raj = new Raj { WomenArePresent = false};
+
+            //Act
+
+            //Assert
+            Assert.AreEqual("Hello friends", raj.Speak());
+        }
+
+        [TestMethod]
+        public void ActorMixins_Raj_WomenPresent_Speaks()
+        {
+            //Arrange
+            Actor raj = new Raj { WomenArePresent = true };
+
+            //Act
+
+            //Assert
+            Assert.AreEqual("mumble", raj.Speak());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ActorMixins_NullActor_ThrowsException()
+        {
+            //Arrange
+
+            //Act
+            Actor nullActor = new Actor();
+
+            //Assert
+        }
+    }
+}
