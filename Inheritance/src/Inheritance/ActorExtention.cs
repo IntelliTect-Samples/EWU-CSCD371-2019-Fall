2b9903321b17d720467fe96db6d0248c3007c048
Joel <joelkalich1111@gmail.com>
2019-10-22 11:56:46 -0700

    Updated to use switch statement

--- Inheritance/src/Inheritance/ActorExtention.cs
+++ Inheritance/src/Inheritance/ActorExtention.cs
@@ -4,17 +4,23 @@ namespace Inheritance {
     public static class ActorExtention {
         public static String Speak(Actor actor) {
             string result = "";
-            if (actor is Raj) {
-                Raj raj = (Raj) actor;
-                if (raj.womenArePresent) {
-                    result = "*Incoherent mumbles*";
-                } else {
-                    result = "I'm so nerdy and awkward, it's hilarious!";
-                }
-            } else if (actor is Sheldon) {
-                result = "I really wish the programmer knew more about Big Bang Therory, so he could better make fun of me!";
-            } else if (actor is Penny) {
-                result = "If the programmer isn't mistaken, I'm just the token hot chick.";
+            switch (actor) {
+                case Sheldon sheldon:
+                    result = "I really wish the programmer knew more about Big Bang Therory, so he could better make fun of me!";
+
+                    break;
+                case Penny penny:
+                    result = "If the programmer isn't mistaken, I'm just the token hot chick.";
+
+                    break;
+                case Raj raj:
+                    raj = (Raj) actor;
+                    if (raj.womenArePresent) {
+                        result = "*Incoherent mumbles*";
+                    } else {
+                        result = "I'm so nerdy and awkward, it's hilarious!";
+                    }
+                    break;
             }
             result += " *canned laughter*";
             return result;
