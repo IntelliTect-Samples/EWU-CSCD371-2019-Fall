b43acf91948f7f5f4f993078885e885cf29007c7
Austin Hall <Haustin7798@gmail.com>
2019-10-22 00:18:44 -0700

    Made changes to Television on Tests
    I had previously made size be the first thing printed, when it should've been manufacturer like the instructions said

--- Inheritance/test/Inheritance.Tests/PrinterTests.cs
+++ Inheritance/test/Inheritance.Tests/PrinterTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Inheritance;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -7,10 +8,10 @@ using System.Text;
 namespace Inheritance.Tests
 {
     [TestClass]
-    public class PersonPrinterTests
+    public class PrinterTests
     {
         [TestMethod]
-        public void PersonGetsPrinted()
+        public void ItemGetsPrinted()
         {
             // Arrange
             var item = new TestItem { Name = "Test Item" };
@@ -34,9 +35,80 @@ namespace Inheritance.Tests
                 }
             }
         }
-    }
 
-    public class TestItem : Item {
-        public string Name { get; set; }
-    }
+[TestMethod]
+[ExpectedException(typeof(ArgumentNullException))]
+public void Television_PrintInfo_SizeNull()
+{
+    //Arrange
+    Television television = new Television("LG", null);
+
+    //Act
+    television.PrintInfo();
 }
+[TestMethod]
+[ExpectedException(typeof(ArgumentNullException))]
+public void Television_PrintInfo_ManufacturerNull()
+{
+    //Arrange
+    Television television = new Television(null, "60");
+
+    //Act
+    television.PrintInfo();
+}
+
+[TestMethod]
+public void Television_PrintInfoCorrectly()
+{
+    //Arrange
+    Television television = new Television("LG", "60");
+
+    //Act
+    television.PrintInfo();
+
+    //Assert
+    Assert.AreEqual(television.PrintInfo(), "<LG> - <60>");
+}
+
+[TestMethod]
+[ExpectedException(typeof(ArgumentNullException))]
+public void Food_PrintInfo_Brandnull()
+{
+    //Arrange
+    Food food = new Food("123456789", null);
+
+    //Act
+    food.PrintInfo();
+}
+[TestMethod]
+[ExpectedException(typeof(ArgumentNullException))]
+public void Food_PrintInfo_UpcNull()
+{
+    //Arrange
+    Food food = new Food(null, "Kellogs");
+
+    //Act
+    food.PrintInfo();
+}
+
+[TestMethod]
+public void Food_PrintInfoCorrectly()
+{
+    //Arrange
+    Food food = new Food("123456789", "Kellogs");
+
+    //Act
+    food.PrintInfo();
+
+    //Assert
+    Assert.AreEqual(food.PrintInfo(), "<123456789> - <Kellogs>");
+}
+     }
+
+     public class TestItem : Item
+{
+    public string Name { get; set; }
+
+    public override string PrintInfo() => Name;
+}
+ }
