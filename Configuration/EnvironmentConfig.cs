be5f7c3d102cd48f6a634663515c2662de506e9f
Jake <jmcgillicuddy@eagles.ewu.edu>
2019-10-23 08:23:31 -0700

    create EnvironmentConfig class

---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration
+{
+    class EnvironmentConfig : IConfig
+    {
+        public bool GetConfigValue(string name, string? value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
