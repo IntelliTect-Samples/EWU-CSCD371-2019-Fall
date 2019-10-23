493090039de2cec857b2d347397ace4b86729fbd
Asher Mancinelli <ashermancinelli@gmail.com>
2019-10-23 14:18:16 -0700

    Passing file creation tests
    Added streamwriters/readers for FileConfig.
    First commit passing all tests.

--- Configuration.Tests/FileConfigTests.cs
+++ Configuration.Tests/FileConfigTests.cs
@@ -11,12 +11,15 @@ namespace Configuration.Tests
         public void FileConfig_CreatesFile_Success()
         {
             string fileName = "somefilename.txt";
+            string name = "SomeName", value = "SomeValue";
             try 
             {
                 var sut = new FileConfig()
                 {
                     FileName = fileName
                 };
+
+                sut.SetConfigValue(name, value);
 
                 Assert.IsTrue(File.Exists(Environment.CurrentDirectory + fileName));
             }
--- Configuration/FileConfig.cs
+++ Configuration/FileConfig.cs
@@ -7,19 +7,33 @@ namespace Configuration
     {
         public string FileName { get; set; }
 
+        private string FormatConfig(string name, string value) =>
+            $"{name}={value}";
+
         public override bool GetConfigValue(string name, out string? value)
         {
             using (var sr = new StreamReader(Environment.CurrentDirectory + FileName))
             {
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] values = line.Split('=');
+                    if (values[0] == name)
+                    {
+                        value = values[1];
+                        return true;
+                    }
+                }
+                value = null;
+                return false;
             }
-            value = null;
-            return true;
         }
 
         public override bool SetConfigValue(string name, string? value)
         {
-            using (var sw = new StreamWriter(Environment.CurrentDirectory + FileName))
+            using (var sw = new StreamWriter(Environment.CurrentDirectory + FileName, append: true))
             {
+                sw.WriteLine(FormatConfig(name, value)); 
             }
             return true;
         }
