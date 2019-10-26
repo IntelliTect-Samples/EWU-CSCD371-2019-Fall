3438fb489cf89b34f5f38c69dda58d79946e9ee4
Khanh Luu <nessy_boy3000@yahoo.com>
2019-10-25 19:44:52 -0700

    in-progress v.1

---
+++ Configuration.Tests/EnvironmentConfigTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Configuration.Tests
+{
+    [TestClass]
+    class EnvironmentConfigTests
+    {
+        [TestMethod]
+        public void EnvironmentConfig_AssignValue_ReturnTrue()
+        {
+            string nameTest = "test name";
+            string valueTest = "test value";
+            bool check;
+
+            EnvironmentConfig environmentConfig = new EnvironmentConfig();
+
+            check = environmentConfig.GetConfigValue(nameTest, valueTest);
+
+            Assert.IsTrue(check);
+
+        }
+    }
+}
