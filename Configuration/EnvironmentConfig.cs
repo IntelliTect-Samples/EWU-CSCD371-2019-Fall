0f7b65e60b93ccba83635b1f2b734f066bb53d3c
Josh Lini <networkmisc@gmail.com>
2019-10-26 14:45:36 -0700

    Created EnvironmentConfig. Made out param in IConfig. Added nullable.

---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration
+{
+    class EnvironmentConfig : IConfig
+    {
+        public bool GetConfigValue(string name, out string? value)
+        {
+            try
+            {
+                value = Environment.GetEnvironmentVariable(name);
+            }
+            catch (ArgumentNullException)
+            {
+                value = null;
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            try
+            {
+                if (name is null || name.Contains(' ', 0)) { throw new ArgumentException(name); }
+                Environment.SetEnvironmentVariable(name, value);
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
