ecc491830e2d335e2c169929f969304b4e2f9f39
Jacob Berger <jberger.1919@gmail.com>
2019-10-26 16:20:38 -0700

    WIP
    Trying to figure out what I'm doing before work. Have added FileConfig and EnvironmentConfig, working on SampleApp

---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Configuration
+{
+    public class EnvironmentConfig : IConfig
+    {
+        private List<string> valueNames;
+
+        public EnvironmentConfig()
+        {
+            valueNames = new List<string>();
+        }
+
+        public bool GetConfigValue(string name, string? value)
+        {
+            value = Environment.GetEnvironmentVariable(name);
+            if (value is null)
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            if (value is null)
+            {
+                return false;
+            } else
+            {
+                valueNames.Add(name);
+                Environment.SetEnvironmentVariable(name, value);
+                return true;
+            }
+        }
+
+        ~EnvironmentConfig()
+        {
+            foreach(string name in valueNames)
+            {
+                Environment.SetEnvironmentVariable(name, null);
+            }
+        }
+    }
+}
---
+++ Configuration/FileConfig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Configuration
+{
+    public class FileConfig
+    {
+        private string path;
+
+        public FileConfig(string path)
+        {
+            this.path = path;
+        }
+
+        public void WriteConfig(string name, string? value)
+        {
+            if (name is null || value is null)
+            {
+                throw new ArgumentNullException("Name or value cannot be null");
+            } else if (!CheckValidArguments(name, value))
+            {
+                throw new ArgumentException("Invalid argument passed in");
+            }
+
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine($"{name}={value}");
+            }
+        }
+
+        public List<Tuple<string, string?>> ReadConfig()
+        {
+            string[] file = File.ReadLines(path).ToArray();
+            List<Tuple<string, string?>> tuples = new List<Tuple<string, string?>>();
+            foreach(string line in file)
+            {
+                string[] values = line.Split('=');
+                tuples.Add(new Tuple<string, string?>(values[0], values[1]));
+            }
+
+            return tuples;
+        }
+
+        private bool CheckValidArguments(string name, string value)
+        {
+            if (name.Contains('=') || name.Contains(' ') || value.Contains('=') || value.Contains(' ') || name.Length == 0 || value.Length == 0)
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+
+        public 
+    }
+}
