af647e8ef263cde671d30c3c0e2f6b2684c7768c
Doug Doner <ddoner02@gmail.com>
2019-10-26 01:05:15 -0700

    Adds EnvironmentConfig and tests

---
+++ Configuration.Tests/EnvironentConfigTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration.Tests
+{
+    [TestClass]
+    public class EnvironentConfigTests
+    {
+        [TestMethod]
+        [DataRow("ProgramData", "C:\\ProgramData")]
+        [DataRow("Invalid row", null)]
+        [DataRow("Public", "C:\\Users\\Public")]
+        public void GetEnvironment_settingExists(string name, string? value)
+        {
+            //Organize
+            IConfig environmentConfig = new EnvironmentConfig();
+
+
+            //Act
+            bool environmentVariableExists = environmentConfig.GetConfigValue(name, value);
+            //Assert
+            Assert.IsTrue(environmentVariableExists);
+
+        }
+
+        [TestMethod]
+        [DataRow("Test", "hello")]
+        [DataRow("ProgramData", "C:\\ProgramData2")]
+        [DataRow("Invalid row", null)]
+        public void SetEnvironment_SetsNewVariable(string name, string? value)
+        {
+            //Organize
+            IConfig environmentConfig = new EnvironmentConfig();
+            //Act
+            environmentConfig.SetConfigValue(name, value);
+            //Assert
+            Assert.AreEqual(value, Environment.GetEnvironmentVariable(name));
+        }
+    }
+}
---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration
+{
+    public class EnvironmentConfig : IConfig
+    {
+        public bool GetConfigValue(string name, string? value)
+        {
+            return value == Environment.GetEnvironmentVariable(name);
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            Environment.SetEnvironmentVariable(name, value);
+            return GetConfigValue(name, value);
+        }
+    }
+}
