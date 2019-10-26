e44b970ee95e92bdf6d39bc3e047d572523a0f02
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-10-26 14:53:22 -0700

    Implement FileConfig

---
+++ Configuration/FileConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Configuration
+{
+    public class FileConfig : IConfig
+    {
+        private string Filename { get; }
+
+        public FileConfig(string filename)
+        {
+            Filename = filename;
+        }
+
+        public bool GetConfigValue(string name, out string? value)
+        {
+            if (name is null) throw new ArgumentNullException(nameof(name));
+            if (name.Length == 0) throw new ArgumentException($"{nameof(name)} cannot be empty.", nameof(name));
+            if (name.Contains(" ")) throw new ArgumentException($"{nameof(name)} cannot contain spaces.", nameof(name));
+            if (name.Contains("=")) throw new ArgumentException($"{nameof(name)} cannot contain equal signs.", nameof(name));
+
+            value = File.Exists(Filename) switch
+            {
+                true => (from line in File.ReadAllLines(Filename)
+                         let split = line.Split('=')
+                         select (name: split[0], value: split[1]))
+                        .FirstOrDefault(setting => setting.name == name).value,
+                false => null
+            };
+
+            return !(value is null);
+        }
+
+        public bool SetConfigValue(string name, string value)
+        {
+            if (name is null) throw new ArgumentNullException(nameof(name));
+            if (name.Length == 0) throw new ArgumentException($"{nameof(name)} cannot be empty.", nameof(name));
+            if (name.Contains(" ")) throw new ArgumentException($"{nameof(name)} cannot contain spaces.", nameof(name));
+            if (name.Contains("=")) throw new ArgumentException($"{nameof(name)} cannot contain equal signs.", nameof(name));
+
+            if (value is null) throw new ArgumentNullException(nameof(value));
+            if (value.Length == 0) throw new ArgumentException($"{nameof(value)} cannot be empty.", nameof(value));
+            if (value.Contains(" ")) throw new ArgumentException($"{nameof(value)} cannot contain spaces.", nameof(value));
+            if (value.Contains("=")) throw new ArgumentException($"{nameof(value)} cannot contain equal signs.", nameof(value));
+
+            IDictionary<string, string> settings = File.Exists(Filename) switch
+            {
+                true => (from line in File.ReadAllLines(Filename)
+                         let split = line.Split('=')
+                         select (name: split[0], value: split[1]))
+                        .ToDictionary(setting => setting.name, setting => setting.value),
+                false => new Dictionary<string, string>()
+            };
+
+            settings[name] = value;
+
+            File.WriteAllLines(Filename, settings.Select(item => $"{item.Key}={item.Value}"));
+            return true;
+        }
+}
+}
