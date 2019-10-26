1ae4b4d9d24672987586f33444dd56b07e3d066e
Andrew Sales <dnisyndrum@yahoo.com>
2019-10-26 16:14:59 -0700

    Implimented EnvironmentConfigs class and added classes for testing FileConfig and EnvironmentConfig classes.

---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration
+{
+    public class EnvironmentConfig : IConfig
+    {
+        private List<string> ConfigNames = new List<string>();
+
+        public bool GetConfigValue(string name, string? value)
+        {
+            value = Environment.GetEnvironmentVariable(name);
+            if(value is null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            if(value is null)
+            {
+                return false;
+            }
+            else
+            {
+                ConfigNames.Add(name);
+                Environment.SetEnvironmentVariable(name, value);
+            }
+            return true;
+        }
+
+        ~EnvironmentConfig()
+        {
+            foreach(string name in ConfigNames)
+            {
+                Environment.SetEnvironmentVariable(name, null);
+            }
+        }
+
+    }
+}
