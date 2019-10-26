1f527f75efe18072e6f0beaa463578f993cb3db9
Andrew Sales <dnisyndrum@yahoo.com>
2019-10-26 16:31:23 -0700

    Added SampleAppTests project.

---
+++ SampleApp.Tests/SampleAppTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleApp.Tests
+{
+    [TestClass]
+    public class SampleAppTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
