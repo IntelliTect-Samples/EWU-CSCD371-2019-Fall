b4f19b27073d4ef3371d9ce12249abd90eb45172
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-10-26 01:28:26 -0700

    Implement EnvironmentConfig

---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Configuration
+{
+    public class EnvironmentConfig : IConfig
+    {
+        public bool GetConfigValue(string name, out string? value)
+        {
+            value = Environment.GetEnvironmentVariable(name);
+            return !(value is null);
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            if (name is null) throw new ArgumentNullException(nameof(name));
+            try
+            {
+                Environment.SetEnvironmentVariable(name, value);
+                return true;
+            }
+            catch (System.Security.SecurityException)
+            {
+                return false;
+            }
+        }
+    }
+}
