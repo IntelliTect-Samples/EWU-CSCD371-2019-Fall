c51179cd76180dc897d2633b63821cdbfb5addd5
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-10-26 15:25:33 -0700

    Update EnvironmentConfig to pass new tests

--- Configuration/EnvironmentConfig.cs
+++ Configuration/EnvironmentConfig.cs
@@ -6,13 +6,26 @@ namespace Configuration
     {
         public bool GetConfigValue(string name, out string? value)
         {
-            value = Environment.GetEnvironmentVariable(name);
-            return !(value is null);
+            if (name is null) throw new ArgumentNullException(nameof(name));
+            if (name.Length == 0) throw new ArgumentException($"{nameof(name)} cannot be empty.", nameof(name));
+            if (name.Contains(" ")) throw new ArgumentException($"{nameof(name)} cannot contain spaces.", nameof(name));
+            if (name.Contains("=")) throw new ArgumentException($"{nameof(name)} cannot contain equal signs.", nameof(name));
+
+            return !((value = Environment.GetEnvironmentVariable(name)) is null);
         }
 
         public bool SetConfigValue(string name, string value)
         {
             if (name is null) throw new ArgumentNullException(nameof(name));
+            if (name.Length == 0) throw new ArgumentException($"{nameof(name)} cannot be empty.", nameof(name));
+            if (name.Contains(" ")) throw new ArgumentException($"{nameof(name)} cannot contain spaces.", nameof(name));
+            if (name.Contains("=")) throw new ArgumentException($"{nameof(name)} cannot contain equal signs.", nameof(name));
+
+            if (value is null) throw new ArgumentNullException(nameof(value));
+            if (value.Length == 0) throw new ArgumentException($"{nameof(value)} cannot be empty.", nameof(value));
+            if (value.Contains(" ")) throw new ArgumentException($"{nameof(value)} cannot contain spaces.", nameof(value));
+            if (value.Contains("=")) throw new ArgumentException($"{nameof(value)} cannot contain equal signs.", nameof(value));
+
             try
             {
                 Environment.SetEnvironmentVariable(name, value);
