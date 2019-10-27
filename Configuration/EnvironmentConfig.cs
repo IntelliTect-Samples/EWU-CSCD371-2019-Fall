07b8984e76108488fef0165d93c517655e38d531
Joel <joelkalich1111@gmail.com>
2019-10-27 14:40:45 -0700

    Implemented EnvironmentConfig class

---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration {
+    class EnvironmentConfig : IConfig {
+        public bool GetConfigValue(string name, out string? value) {
+            value = null;
+            try {
+                value = Environment.GetEnvironmentVariable(name);
+            } catch (ArgumentNullException) {
+            }
+            if (value is null) {
+                return false;
+            }
+            return true;
+        }
+
+        public bool SetConfigValue(string name, string? value) {
+            if (Environment.GetEnvironmentVariables().Contains(name)) {
+                try {
+                    Environment.SetEnvironmentVariable(name, value);
+                } catch (ArgumentNullException) {
+                    return false;
+                } catch (ArgumentException) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
