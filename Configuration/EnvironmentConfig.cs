756c7b52f5e5f98933b046e69dde312d7e10438e
Joel <joelkalich1111@gmail.com>
2019-10-27 16:09:00 -0700

    Implemented FileConfig

--- Configuration/EnvironmentConfig.cs
+++ Configuration/EnvironmentConfig.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 namespace Configuration {
     class EnvironmentConfig : IConfig {
+        private List<String> names;
+
         public bool GetConfigValue(string name, out string? value) {
             value = null;
             try {
@@ -16,17 +18,33 @@ namespace Configuration {
             return true;
         }
 
+        internal List<string> getNames() {
+            return this.names;
+        }
+
         public bool SetConfigValue(string name, string? value) {
-            if (Environment.GetEnvironmentVariables().Contains(name)) {
+            if (InputIsValid(name, value)) {
                 try {
                     Environment.SetEnvironmentVariable(name, value);
+                    names.Add(name);
                 } catch (ArgumentNullException) {
                     return false;
                 } catch (ArgumentException) {
                     return false;
                 }
+                return true;
             }
-            return true;
+            return false;
+        }
+
+        private static bool InputIsValid(string? name, string? value) {
+            return !(name is null || name is "" || value is null || value is "" || name.Contains("=") || name.Contains(" "));
+        }
+
+        ~EnvironmentConfig() {
+            foreach (string name in names) {
+                Environment.SetEnvironmentVariable(name, null);
+            }
         }
     }
 }
--- Configuration/FileConfig.cs
+++ Configuration/FileConfig.cs
@@ -1,9 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace Configuration {
     class FileConfig {
-        
+        private string path;
+        public FileConfig(string path) {
+            this.path = path;
+        }
+
+        public void WriteToFile(EnvironmentConfig config) {
+            List<string> names = config.getNames();
+            using (StreamWriter sw = new StreamWriter(this.path, false)) {
+                foreach (string name in names) {
+                    sw.WriteLine($"{name}={Environment.GetEnvironmentVariable(name)}");
+                }
+            }
+        }
+
+        public List<string[]> ReadFromFile() {
+            List<string[]> result = new List<String[]>();
+            string[] lines = File.ReadAllLines(this.path);
+            foreach (string line in lines) {
+                result.Add(line.Split("="));
+            }
+            return result;
+        }
     }
 }
