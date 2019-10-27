5efee80d4828dea0c7d52fd66f1da7229964f914
Andrew Sales <dnisyndrum@yahoo.com>
2019-10-27 16:48:10 -0700

    added MockConfig and test projects

---
+++ MockConfig/MockConfig.cs
@@ -0,0 +1,26 @@
+﻿using Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace MockConfig
+{
+    public class MockConfig : IConfig
+    {
+        private List<string> ConfigNames = new List<string>();
+
+        public MockConfig()
+        {
+            
+        }
+
+        public bool GetConfigValue(string name, out string? value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SampleApp.Tests/MockConfigTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp.Tests
+{
+    [TestClass]
+    public class MockConfigTests
+    {
+    }
+}
