bfa82cff0e2c16af428c86521f15af52fc31f423
Josh Lini <networkmisc@gmail.com>
2019-10-28 16:22:09 -0700

    wrote and begun testing FileConfig

---
+++ Configuration.Tests/FileConfigTests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration.Tests
+{
+    [TestClass]
+    public class FileConfigTests
+    {
+        [TestMethod]
+        public void SetConfigValue_GivenValidNameAndValue_ReturnsTrue()
+        {
+            //Arrange
+            FileConfig fileConf = new Configuration.FileConfig();
+
+            //Act
+            bool worked = fileConf.SetConfigValue("variable1", "true");
+
+            //Assert
+            Assert.IsTrue(worked);
+        }
+
+        [TestMethod]
+        public void SetConfigValue_GivenInvalidName_ReturnsFalse()
+        {
+            //Arrange
+            FileConfig fileConf = new Configuration.FileConfig();
+
+            //Act
+            bool worked = fileConf.SetConfigValue("variable 1", "true");
+
+            //Assert
+            Assert.IsFalse(worked);
+        }
+
+        [TestMethod]
+        public void SetConfigValue_GivenValidName_NullValue_ReturnsFalse()
+        {
+            //Arrange
+            FileConfig fileConf = new Configuration.FileConfig();
+
+            //Act
+            bool worked = fileConf.SetConfigValue("variable1", null);
+
+            //Assert
+            Assert.IsFalse(worked);
+        }
+
+        [TestMethod]
+        public void GetConfigValue_GivenValidName_ReturnsTrue()
+        {
+            //Arrange
+            FileConfig fileConf = new Configuration.FileConfig();
+            string? output;
+
+            //Act
+            fileConf.SetConfigValue("variable1", "true");
+            bool worked = fileConf.GetConfigValue("variable1", out output);
+
+            //Assert
+            Assert.IsTrue(worked);
+            Assert.AreEqual(output, "true");
+        }
+
+        [TestMethod]
+        public void GetConfigValue_GivenInvalidName_ReturnsFalse()
+        {
+            //Arrange
+            FileConfig fileConf = new Configuration.FileConfig();
+
+            //Act
+            bool worked = fileConf.GetConfigValue("variable 1", out _);
+
+            //Assert
+            Assert.IsFalse(worked);
+        }
+
+        [TestMethod]
+        public void GetConfigValue_GivenValidButUnsetName_ReturnsFalse()
+        {
+            //Arrange
+            FileConfig fileConf = new Configuration.FileConfig();
+
+            //Act
+            bool worked = fileConf.GetConfigValue("thisistexas", out _);
+
+            //Assert
+            Assert.IsFalse(worked);
+        }
+    }
+}
--- Configuration/FileConfig.cs
+++ Configuration/FileConfig.cs
@@ -1,19 +1,98 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Configuration
 {
-    class FileConfig : IConfig
+    public class FileConfig : IConfig
     {
+        private string settingsPath = "config.settings";
+
         public bool GetConfigValue(string name, out string? value)
         {
-            throw new NotImplementedException();
+            if (!isValidName(name) || !File.Exists(settingsPath))
+            {
+                value = null;
+                return false;
+            }
+
+            foreach(string setting in File.ReadAllLines(settingsPath))
+            {
+                if (setting.StartsWith(name, StringComparison.CurrentCulture))
+                {
+                    string[] nameAndValue = setting.Split("=");
+                    if (nameAndValue.Length != 2)
+                    {
+                        throw new FormatException("Invalid Setting In " + settingsPath);
+                    }
+                    else
+                    {
+                        value = nameAndValue[1];
+                        return true;
+                    }
+                }
+            }
+
+            value = null;
+            return false;
         }
 
         public bool SetConfigValue(string name, string? value)
         {
-            throw new NotImplementedException();
+            if (!isValidName(name)) { return false; }
+
+            Hashtable settingsHashTable = new Hashtable();
+
+            if (File.Exists(settingsPath))
+            {
+                string[] fileText = File.ReadAllLines(settingsPath);
+                
+                foreach(string setting in fileText)
+                {
+                    string[] nameAndValue = setting.Split('=');
+                    if (nameAndValue.Length != 2) { throw new FormatException("Invalid Setting In " + settingsPath); }
+
+                    try
+                    {
+                        settingsHashTable.Add(nameAndValue[0], nameAndValue[1]);
+                    }
+                    catch (ArgumentException)
+                    {
+                        throw new FormatException("Duplicate Entry In " + settingsPath);
+                    }
+                }
+            }
+
+            settingsHashTable.Remove(name);
+            settingsHashTable.Add(name, value);
+
+            string[] keys = new string[settingsHashTable.Count];
+            string[] values = new string[settingsHashTable.Count];
+            string[] keyValuePairs = new string[settingsHashTable.Count];
+
+            settingsHashTable.Keys.CopyTo(keys, 0);
+            settingsHashTable.Values.CopyTo(values, 0);
+
+            for (int i = 0; i < keyValuePairs.Length; i++)
+            {
+                keyValuePairs[i] = keys[i] + '=' + values[i];
+            }
+
+            File.WriteAllLines(settingsPath, keyValuePairs);
+            
+            return true;
+        }
+
+        private static bool isValidName(string name)
+        {
+            if (string.IsNullOrEmpty(name) || name.Contains(' ', StringComparison.CurrentCulture) || name.Contains('=', StringComparison.CurrentCulture))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
