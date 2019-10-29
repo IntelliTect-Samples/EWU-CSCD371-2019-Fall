49997675b972566a2d23823c7754f14ab0afa54f
Dran <Theo.pirtle7@gmail.com>
2019-10-29 07:46:51 -0700

    Assignment4 and unit test are complete

---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration
+{
+    public class EnvironmentConfig : IConfig
+    {
+        public bool GetConfigValue(string name, string? value)
+        {
+            value = Environment.GetEnvironmentVariable(name);
+
+            return (value != null);
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            Environment.SetEnvironmentVariable(name, value);
+            return true;
+        }
+    }
+}
---
+++ Configuration/FileConfig.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Configuration
+{
+    public class FileConfig
+    {
+
+        private string _FilePath = Environment.CurrentDirectory+Path.DirectorySeparatorChar+"config.settings";
+
+        public FileConfig()
+        {
+
+        }
+
+        public Dictionary<string,string> FileRead()
+        {
+            StreamReader reader = new StreamReader(_FilePath);
+            Dictionary<string,string> map = new Dictionary<string, string>();
+            while (true)
+            {
+                string line = reader.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+                string[] br = line.Split("=");
+
+                map.Add(br[0], br[1]);
+            }
+            return map;
+        }
+
+        public bool FileWrite(string type,string value)
+        {
+            StreamWriter writer = new StreamWriter(_FilePath);
+
+            if (type == null || type == "" || type.Contains("=") || type.Contains(" "))
+            {
+                return false;
+            }
+
+            if (value != null && value != "" && value.Contains("=") && value.Contains(" "))
+            {
+                return false;
+            }
+
+            writer.WriteLine($"{type}={value}");
+
+
+            return true;
+        }
+    }
+}
