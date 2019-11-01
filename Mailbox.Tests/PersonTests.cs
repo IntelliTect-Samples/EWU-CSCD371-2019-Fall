10178dde413f571aa5c29eb9592bb0cb18115610
Joel <joelkalich1111@gmail.com>
2019-11-01 13:02:21 -0700

    Arranged unit testing classes

---
+++ Mailbox.Tests/PersonTests.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mailbox.Tests {
+    [TestClass]
+    class PersonTests {
+    }
+}
--- Mailbox/Sizes.cs
+++ Mailbox/Sizes.cs
@@ -3,7 +3,7 @@
 namespace Mailbox {
     [Flags]
     public enum Sizes {
-        Defult = 0b_0000,
+        Default = 0b_0000,
         Small = 0b_0001,
         Medium = 0b_0010,
         Large = 0b_0100,
