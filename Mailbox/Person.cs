98a367f6d80e0582fb01d1bc80d975923dc51ad9
Josh Lini <networkmisc@gmail.com>
2019-11-02 16:55:18 -0700

    Enum and Person struct

--- Mailbox/Person.cs
+++ Mailbox/Person.cs
@@ -1,4 +1,54 @@
-﻿namespace Mailbox
-{
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
+namespace Mailbox
+{
+    public struct Person : IEquatable<Person>
+    {
+        private (string firstName, string lastName) name;
+
+        public Person(string firstName, string lastName)
+        {
+            if(firstName is null) { throw new ArgumentNullException(firstName, nameof(firstName) + " may not be null"); }
+            if (lastName is null) { throw new ArgumentNullException(lastName, nameof(lastName) + " may not be null"); }
+
+            name.firstName = firstName;
+            name.lastName = lastName;
+        }
+        public bool Equals(Person other)
+        {
+            return name.firstName == other.getName().first
+                && name.lastName == other.getName().last;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj); //TODO
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode(); //TODO
+        }
+
+        public (string first, string last) getName()
+        {
+            return name;
+        }
+
+        public static bool operator ==(Person a, Person b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Person a, Person b)
+        {
+            return !a.Equals(b);
+        }
+
+        public override string ToString()
+        {
+            return $"{name.lastName}, {name.firstName}";
+        }
+    }
 }
--- Mailbox/Size.cs
+++ Mailbox/Size.cs
@@ -1,4 +1,14 @@
-﻿namespace Mailbox
-{
+﻿using System;
 
+namespace Mailbox
+{
+    [Flags]
+    public enum Sizes
+    {
+        undefined = 0,
+        small = 1,
+        medium = 2,
+        large = 4,
+        premium = 8
+    }
 }
