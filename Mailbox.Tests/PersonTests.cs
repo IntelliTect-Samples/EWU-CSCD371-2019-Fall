4ef3e43dfadad32211d5d9d583bf1f8dd64589b4
Josh Lini <networkmisc@gmail.com>
2019-11-02 17:52:22 -0700

    Working on Mailbox

---
+++ Mailbox.Tests/PersonTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mailbox.Tests
+{
+    [TestClass]
+    class PersonTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PersonGivenNull_ThrowsArgNullEx()
+        {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type. This is a Unit Test
+            var _ = new Person(null, null);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+        }
+    }
+}
--- Mailbox/Mailbox.cs
+++ Mailbox/Mailbox.cs
@@ -1,4 +1,22 @@
 ﻿namespace Mailbox
 {
+    public class Mailbox
+    {
+        public Sizes Size { get; }
+        public (int X, int Y) Location { get; }
+        public Person Owner { get; }
 
+        public Mailbox(Sizes size, (int X, int Y) location, Person owner)
+        {
+            //None of the parameters could be null
+            Size = size;
+            Location = location;
+            Owner = owner;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();//TODO
+        }
+    }
 }
