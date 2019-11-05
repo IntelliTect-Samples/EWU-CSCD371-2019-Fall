88ec40e00ee32fb1578a931309852f7b0d85172a
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-11-04 21:33:03 -0800

    Implement Mailbox

--- Mailbox/Mailbox.cs
+++ Mailbox/Mailbox.cs
@@ -1,8 +1,24 @@
-﻿namespace Mailbox
+﻿using System;
+
+namespace Mailbox
 {
     public class Mailbox
     {
         public (int X, int Y) Location { get; }
+
         public Person Owner { get; }
+
+        public Sizes Size { get; }
+
+        public Mailbox((int X, int Y) location, Person owner, Sizes size)
+        {
+            if (!size.IsValid()) throw new ArgumentException("Invalid size.", nameof(size));
+            Location = location;
+            Owner = owner;
+            Size = size;
+        }
+
+        public override string? ToString() =>
+            $"{Owner.FirstName} {Owner.LastName}'s{(Size.Premium() ? " Premium" : "")} {Size.Size()} mailbox at {Location}";
     }
 }
