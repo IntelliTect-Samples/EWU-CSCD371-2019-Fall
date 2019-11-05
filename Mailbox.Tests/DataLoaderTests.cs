86b658992fa1fd3a3481630588fd0ece9c12039b
Doug Doner <ddoner02@gmail.com>
2019-11-05 13:00:25 -0800

    Differentiates between load and save tests
    For DataLoaderTests

--- Mailbox.Tests/DataLoaderTests.cs
+++ Mailbox.Tests/DataLoaderTests.cs
@@ -28,11 +28,19 @@ namespace Mailbox.Tests
         {
             using (FileStream fileStream = new FileStream("testfile.txt", FileMode.OpenOrCreate))
             {
+
                 DataLoader dataLoader = new DataLoader(fileStream);
 
-                List<Mailbox> mailboxList = dataLoader.Load();
+                List<Mailbox> mailboxList = new List<Mailbox>();
 
-                Assert.AreNotEqual(0, mailboxList.Count);
+                mailboxList.Add(new Mailbox() { Owner = new Person("Doug", "Student"), Location = (2, 4), Size = Size.Medium });
+                mailboxList.Add(new Mailbox() { Owner = new Person("James", "Student"), Location = (2, 4), Size = Size.Medium });
+
+                dataLoader.Save(mailboxList);
+
+                var testMailboxList = dataLoader.Load();
+
+                Assert.AreEqual(mailboxList.Count, testMailboxList.Count);
 
                 dataLoader.Dispose();
             }
@@ -48,13 +56,14 @@ namespace Mailbox.Tests
                 List<Mailbox> mailboxList = new List<Mailbox>();
 
                 mailboxList.Add(new Mailbox() { Owner = new Person("Doug", "Student") , Location = (2, 4), Size = Size.Medium} );
+                mailboxList.Add(new Mailbox() { Owner = new Person("James", "Student"), Location = (2, 4), Size = Size.Medium });
 
                 dataLoader.Save(mailboxList);
 
                 using var reader = new StreamReader(fileStream, leaveOpen: true);
                 string serialData = reader.ReadToEnd();
 
-                List<Mailbox>? testList = JsonConvert.DeserializeObject<List<Mailbox>> (serialData);
+                List<Mailbox> testList = JsonConvert.DeserializeObject<List<Mailbox>> (serialData);
 
                 Assert.AreEqual(mailboxList.Count, testList.Count);
 
