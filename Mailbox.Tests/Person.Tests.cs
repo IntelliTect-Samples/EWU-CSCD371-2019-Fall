dbb2a11b4f095f8ca79ab52b1a94aec2b709cd11
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-11-05 00:35:56 -0800

    Implement Person tests

---
+++ Mailbox.Tests/Person.Tests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Mailbox.Tests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void Constructor_ValidArgs_CorrectValues()
+        {
+            var fname = "Inigo";
+            var lname = "Montoya";
+
+            var person = new Person(fname, lname);
+
+            Assert.AreEqual(fname, person.FirstName);
+            Assert.AreEqual(lname, person.LastName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_NullFirstName_ThrowsException()
+        {
+#nullable disable
+            new Person(null, "montoya");
+#nullable enable
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_NullLastName_ThrowsException()
+        {
+#nullable disable
+            new Person("Inigo", null);
+#nullable enable
+        }
+
+        [TestMethod]
+        public void Equals_Equal_True()
+        {
+            var person1 = new Person("Inigo", "Montoya");
+            var person2 = new Person("Inigo", "Montoya");
+            Assert.IsTrue(person1 == person2);
+        }
+
+        [TestMethod]
+        public void Equals_NotEqual_False()
+        {
+            var person1 = new Person("Inigo", "Montoya");
+            var person2 = new Person("Miracle", "Max");
+            Assert.IsFalse(person1 == person2);
+        }
+    }
+}
