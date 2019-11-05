8ec6205c0934acff7124592175aef72e3295881c
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-11-04 21:02:28 -0800

    Implement Person

--- Mailbox/Person.cs
+++ Mailbox/Person.cs
@@ -1,7 +1,32 @@
-﻿namespace Mailbox
-{
-    public struct Person
-    {
+﻿using System;
 
+namespace Mailbox
+{
+    public struct Person : IEquatable<Person>
+    {
+        public string FirstName { get; }
+
+        public string LastName { get;  }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override bool Equals(object? obj) =>
+            obj is Person other && Equals(other);
+
+        public bool Equals(Person other) =>
+            (FirstName, LastName) == (other.FirstName, other.LastName);
+
+        public override int GetHashCode() =>
+            (FirstName, LastName).GetHashCode();
+
+        public static bool operator ==(Person left, Person right) =>
+            left.Equals(right);
+
+        public static bool operator !=(Person left, Person right) =>
+            !(left == right);
     }
 }
