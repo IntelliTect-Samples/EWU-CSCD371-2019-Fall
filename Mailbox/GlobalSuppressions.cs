a77a04548654c0ac7534a40fe87484889a5a579e
Jerett Latimer <jerettlatimer@yahoo.com>
2019-11-04 18:58:47 -0800

    Supressed Warnings

---
+++ Mailbox/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:Mailbox.DataLoader.Save(System.Collections.Generic.List{Mailbox.Mailbox})")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>", Scope = "member", Target = "~M:Mailbox.Program.Main(System.String[])")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:Mailbox.Program.Main(System.String[])")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1714:Flags enums should have plural names", Justification = "<Pending>", Scope = "type", Target = "~T:Mailbox.Size")]
--- Mailbox/Person.cs
+++ Mailbox/Person.cs
@@ -3,7 +3,9 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Mailbox
 {
+#pragma warning disable CA1067 // Override Object.Equals(object) when implementing IEquatable<T>
     public struct Person : IEquatable<Person>
+#pragma warning restore CA1067 // Override Object.Equals(object) when implementing IEquatable<T>
     {
         private string? FirstName { get; set; }
         private string? LastName { get; set; }
@@ -37,6 +39,28 @@ namespace Mailbox
         public override string ToString()
         {
             return $"{FirstName} {LastName}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+                return false;
+            return Equals((Person)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (FirstName, LastName).GetHashCode();
+        }
+
+        public static bool operator ==(Person left, Person right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Person left, Person right)
+        {
+            return !(left == right);
         }
     }
 }
