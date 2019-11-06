2f462ff6251b3dc6779849cf40575fece503d007
Asher Mancinelli <ashermancinelli@gmail.com>
2019-11-06 15:33:09 -0800

    Added init structure and tests

---
+++ Assignment6.Tests/ArrayTests.cs
@@ -0,0 +1,43 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment6;
+using System.Collections.Generic;
+
+namespace Assignment6.Tests
+{
+    [TestClass]
+    public class ArrayTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void ArrayConstructor_BadSize_ThrowsException()
+        {
+            var sut = new Array<int>(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.InvalidOperationException))]
+        [DataRow(-1)]
+        [DataRow(1)]
+        public void ArrayIndex_OutOfRange_ThrowsInvalidOperationException(int index)
+        {
+            var sut = new Array<int>();
+            var _ = sut[index];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.InvalidOperationException))]
+        public void ArraySet_OutOfRange_ThrowsException()
+        {
+            var sut = new Array<int>(1);
+            sut[1] = 1;
+        }
+
+        [TestMethod]
+        public void ArraySet_ToList_ReturnsList()
+        {
+            var sut = new Array<int>(1);
+            var listSut = sut.ToList();
+            Assert.IsInstanceOfType(listSut, typeof(List<int>));
+        }
+    }
+}
--- Assignment6/Array.cs
+++ Assignment6/Array.cs
@@ -1,20 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Assignment6
 {
     public class Array<T>
     {
+        private int _Length = 0;
+        private T[]? Data;
+
+        public Array()
+        {
+            _Length = 0;
+        }
+
         public Array(int size)
         {
             if (size < 0)
-                throw new ArgumentOutOfRangeException(nameof(size));
+                throw new System.ArgumentOutOfRangeException(nameof(size));
+            else
+                _Length = size;
+        }
+
+        public void Resize(int length)
+        {
+        }
+
+        public List<T> ToList()
+        {
+            return new List<T>();
+        }
+
+        public T this[int key]
+        {
+            get
+            {
+                if (key >= _Length || key < 0)
+                    throw new System.InvalidOperationException(nameof(key));
+                if (Data[key] is T data)
+                    return data;
+                else
+                    throw new System.InvalidOperationException(nameof(key));
+            }
+            set
+            {
+                if (key >= _Length || key < 0)
+                    throw new System.InvalidOperationException(nameof(key));
+                Data[key] = value;
+            }
         }
 
         public int Length
         {
             get
             {
+                return _Length;
             }
         }
     }
