9e8156a94866f174a7cc7a44f923b0fb40482bdd
TatyanaHubbard <tatsihubbard@gmail.com>
2019-11-07 14:34:31 -0800

    Begin Assignment 6

--- Assignment6/Array.cs
+++ Assignment6/Array.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Assignment6
 {
-    public class Array<T>
+    public class Array<T> : ICollection<T>
     {
-        
+        public int Count => 0;
+
+        public bool IsReadOnly => false;
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Assignment6/ArrayTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment6
+{
+    [TestClass]
+    public class ArrayTests
+    {
+        [TestMethod]
+        public void Array_Add_Success()
+        {
+
+        }
+
+        [TestMethod]
+        public void Array_Clear_Success()
+        {
+
+        }
+
+        [TestMethod]
+        public void Array_Contains_ReturnsItem()
+        {
+
+        }
+
+        [TestMethod]
+        public void Array_Contains_DoesNotContain()
+        {
+
+        }
+
+        [TestMethod]
+        public void Array_Remove_Success()
+        {
+
+        }
+
+        [TestMethod]
+        public void Array_Remove_DoesNotContain()
+        {
+
+        }
+    }
+}
