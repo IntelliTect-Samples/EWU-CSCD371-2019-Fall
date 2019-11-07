56926ca7ebb2bf016b435eaffb0ea38180f8a0af
Jacob Berger <jberger.1919@gmail.com>
2019-11-06 16:28:18 -0800

    Initial work
    WIP

--- Assignment6/Array.cs
+++ Assignment6/Array.cs
@@ -1,10 +1,59 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Assignment6
 {
-    public class Array<T>
+    public class Array<T> : ICollection<T>
     {
-        
+        public int Capacity { get; }
+        public int Index { get; }
+        public Array(int capacity)
+        {
+            if (!(capacity > 0)) {
+                throw new IndexOutOfRangeException("Width must be greater than 0");
+            }
+            Capacity = capacity;
+            Index = 0;
+        }
+
+        public int Count => Index;
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
