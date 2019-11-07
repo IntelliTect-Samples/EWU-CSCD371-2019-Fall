27d180adcfa619b363f09e1ad70eccbbf6acf039
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-11-06 20:45:17 -0800

    Update warning suppressions

--- GlobalSuppressions.cs
+++ GlobalSuppressions.cs
@@ -4,3 +4,5 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "Localization is irrelevant.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1710:Identifiers should have correct suffix", Justification = "Assignment disagrees.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "<Pending>")] 
