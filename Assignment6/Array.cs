4f41874ea9d3775cab74fa9e327722bf82f858f0
Jacob Berger <jberger.1919@gmail.com>
2019-11-06 18:06:41 -0800

    Implemented all but Enum
    Not sure if this is the right approach at this time.

--- Assignment6/Array.cs
+++ Assignment6/Array.cs
@@ -7,7 +7,8 @@ namespace Assignment6
     public class Array<T> : ICollection<T>
     {
         public int Capacity { get; }
-        public int Index { get; }
+        public int Index { get; set; }
+        private T[] Collection { get; set; }
         public Array(int capacity)
         {
             if (!(capacity > 0)) {
@@ -15,35 +16,96 @@ namespace Assignment6
             }
             Capacity = capacity;
             Index = 0;
+            Collection = new T[capacity];
         }
 
         public int Count => Index;
 
-        public bool IsReadOnly => throw new NotImplementedException();
+        public bool IsReadOnly => false;
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            if (item is null)
+            {
+                throw new ArgumentNullException(nameof(item), "Passed in item is null");
+            }
+            if (!(Count == Capacity))
+            {
+                Collection[Index] = item;
+                Index++;
+            }
         }
 
         public void Clear()
-        {
-            throw new NotImplementedException();
+        {   if (Index != 0)
+            {
+                for (int ix = Index; ix >= 0; ix--)
+                {
+                    Collection[Index] = default;
+                }
+            }
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            if (item is null)
+            {
+                throw new ArgumentNullException(nameof(item), "Passed in item is null");
+            }
+
+            if (Index == 0)
+            {
+                return false;
+            }
+
+            foreach (T collectionItem in Collection)
+            {
+                if (collectionItem.Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array), "Array is null");
+            }
+
+            if (arrayIndex < 0 || arrayIndex > Capacity)
+            {
+                throw new IndexOutOfRangeException("Index out of bounds");
+            }
+
+            array[arrayIndex] = Collection[arrayIndex];
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            if (item is null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item is null");
+            }
+
+            if (Index == 0)
+            {
+                return false;
+            }
+
+            for (int ix = 0; ix < Index; ix++)
+            {
+                if (Collection[ix].Equals(item))
+                {
+                    Collection[ix] = default;
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
