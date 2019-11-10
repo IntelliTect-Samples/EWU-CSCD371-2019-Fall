b32ce2ab64a157451561e0724b79b12e56a87d3c
Andrew Sales <dnisyndrum@yahoo.com>
2019-11-09 19:31:09 -0800

    Added functionality for ICollection interface requirements.

--- Assignment6/Array.cs
+++ Assignment6/Array.cs
@@ -1,10 +1,103 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+
+#pragma warning disable CS8653
+#pragma warning disable CS8602
+#pragma warning disable CA2208
+#pragma warning disable CA1303
 
 namespace Assignment6
 {
-    public class Array<T>
+    public class ArrayCollection<T> : ICollection<T>
     {
-        
+        public int Count => _Data.Count;
+        public bool IsReadOnly => false;
+        public int Capacity { get; } 
+        private readonly ICollection<T> _Data;
+        public ArrayCollection(int cap)
+        {
+            if(cap < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(cap));
+            }
+            Capacity = cap;
+            _Data = new List<T>(cap);
+        }
+
+        public ArrayCollection(ICollection<T> collection)
+        {
+            if (collection is null)
+            {
+                throw new ArgumentNullException("Collection is null", nameof(collection));
+            }
+            _Data = new List<T>(collection.Count);
+            Capacity = collection.Count;
+            foreach(T item in collection)
+            {
+                _Data.Add(item);
+            }
+        }
+
+
+        public void Add(T item)
+        {
+            if(Capacity == Count)
+            {
+                throw new ArgumentException("Array at max capacity.", nameof(item));
+            }
+            if(item is null)
+            {
+                throw new ArgumentNullException("Item is null", nameof(item));
+            }
+            _Data.Add(item);
+        }
+
+        public void Clear()
+        {
+            _Data.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            if (item is null)
+            {
+                throw new ArgumentNullException("Item is null", nameof(item));
+            }
+            return _Data.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if(array is null)
+            {
+                throw new ArgumentNullException("Array is null", nameof(array));
+            }
+            if(arrayIndex < 0 || arrayIndex > Capacity)
+            {
+                throw new ArgumentOutOfRangeException("Index is out of bounds of this array.", nameof(arrayIndex));
+            }
+            _Data.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _Data.GetEnumerator();
+        }
+
+        public bool Remove(T item)
+        {
+            if (item is null)
+            {
+                throw new ArgumentNullException("Item is null", nameof(item));
+            }
+            return _Data.Remove(item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _Data.GetEnumerator();
+        }
+
     }
 }
--- Assignment6Tests/ArrayTests.cs
+++ Assignment6Tests/ArrayTests.cs
@@ -1,13 +1,28 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Assignment6.Tests
 {
     [TestClass]
     public class ArrayTests
     {
-        [TestMethod]
-        public void T()
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(-100)]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Array_Constructor_ArgumentOutOfRangeException(int capacity)
         {
+            ArrayCollection<int> array = new ArrayCollection<int>(capacity);
         }
+
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(100)]
+        public void Array_Constructor_Qapla(int capacity)
+        {
+            ArrayCollection<int> array = new ArrayCollection<int>(capacity);
+            Assert.AreEqual(capacity, array.Capacity);
+        }
+
     }
 }
