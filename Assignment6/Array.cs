e8e8c0faf33b6fbb49f3a153adc3e95d4b1f9cee
Joel <joelkalich1111@gmail.com>
2019-11-10 14:36:56 -0800

    Finished (hopefully) the Array class.

--- Assignment6/Array.cs
+++ Assignment6/Array.cs
@@ -1,67 +1,73 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Assignment6 {
-    public class Array<T> : ICollection<T> {
-        private ICollection<T> _Storage;
+    public class Array<Data> : ICollection<Data> {
+        private Data[] _Storage;
         public int Capacity { get; set; }
 
         public Array(int capacity) {
+            if (capacity <= 0) {
+                throw new ArgumentOutOfRangeException("capacity");
+            }
             Capacity = capacity;
-            _Storage = new T[capacity];
+            _Storage = new Data[capacity];
         }
 
-        public int Count => _Storage.Count;
+        public int Count => _Storage.Length;
 
         public bool IsReadOnly => _Storage.IsReadOnly;
 
-        public void Add(T item) {
+        public void Add(Data item) {
             if (item is null) {
-                return;
+                throw new ArgumentNullException("item");
             }
-
+            for (int i = 0; i < _Storage.Length; i++) {
+                if (_Storage[i].Equals(default(Data))) {
+                    _Storage[i] = item;
+                }
+            }
         }
 
         public void Clear() {
-            _Storage = new T[Capacity];
+            _Storage = new Data[Capacity];
         }
 
-        public bool Contains(T item) {
-            if (item is null) {
-                return false;
+        public bool Contains(Data item) {
+            // already throws ArgumentNullException
+            return _Storage.Contains(item);
+        }
+
+        public void CopyTo(Data[] array, int arrayIndex) {
+            if (arrayIndex < 0 || arrayIndex >= _Storage.Length) {
+                throw new IndexOutOfRangeException("Index passed into CoptTo is out of range!");
             }
-            foreach (T stored in _Storage) {
-                if (item.Equals(stored)) {
+            if (array is null) {
+                throw new NullReferenceException("Array passed into CopyTo is null!");
+            }
+            array = new Data[_Storage.Length - arrayIndex];
+            for (int i = arrayIndex, j = 0; i < _Storage.Length; i++, j++) {
+                array[j] = _Storage[i];
+            }
+        }
+
+        public bool Remove(Data item) {
+            if (item is null) {
+                throw new ArgumentNullException("item");
+            }
+            for (int i = 0; i < _Storage.Length; i++) {
+                if (_Storage[i].Equals(item)) {
+                    _Storage[i] = default;
                     return true;
                 }
             }
             return false;
         }
 
-        public void CopyTo(T[] array, int arrayIndex) {
-            throw new NotImplementedException();
-        }
+        public IEnumerator<Data> GetEnumerator() => (IEnumerator<Data>) _Storage.AsEnumerable();
 
-        public IEnumerator<T> GetEnumerator() {
-            throw new NotImplementedException();
-        }
-
-        public bool Remove(T item) {
-            if (item is null) {
-                return false;
-            }
-            foreach (T stored in _Storage) {
-                if (item.Equals(stored)) {
-                    //somehow need to remove item here....
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() {
-            throw new NotImplementedException();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
