d86a3e44a30a27c5b2e87c56f33d98ee68632407
Geoff Brower <fervidusletum@gmail.com>
2019-11-09 17:02:05 -0800

    Initial Setup
    basic project framework

---
+++ Assignment6.Tests/ArrayCollectionTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment6;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment6.Tests
+{
+    [TestClass]
+    public class ArrayCollectionTests
+    {
+        [TestMethod]
+        public void ArrayCollection_Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Assignment6/ArrayCollection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assignment6
+{
+    public class ArrayCollection<T> : ICollection<T> // where T: struct // class // new()
+    {
+        private List<T> Data { get; }
+        private readonly int _Count; // preventing outside classes from modifying count
+        public int Count { get => _Count; } // public facing readonly count
+        public bool IsReadOnly { get; } // only allowed to read data from array
+        public int Capacity { get => Data.Capacity; }
+
+        public ArrayCollection(int capacity, bool isReadOnly = false)
+        {
+            _Count = 0;
+            IsReadOnly = isReadOnly;
+            Data = new List<T>(capacity); // need to add a buffer to prevent cap increase?
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
