14db6bad0ea0e80ace2bcd57c1a5652937d4d429
Josh Lini <networkmisc@gmail.com>
2019-11-10 19:48:28 -0800

    Created Tests for indexing

--- Assignment6Tests/ArrayTests.cs
+++ Assignment6Tests/ArrayTests.cs
@@ -32,5 +32,36 @@ namespace Assignment6Tests
             //Act
             Array<string> _ = new Array<string>(cap);
         }
+
+        [DataTestMethod]
+        [DataRow(0, "hi0")]
+        [DataRow(1, "hi1")]
+        [DataRow(2, "hi2")]
+        [DataRow(3, "hi3")]
+        [DataRow(4, "hi4")]
+        public void ArrayIndexingFunctional(int index, string expected)
+        {
+            //Arrange
+            var array = new Array<string>(5)
+            {
+                //Act
+                [index] = "hi" + index
+            };
+
+            //Assert
+            Assert.AreEqual(array[index], expected);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ArrayThrowsExceptionWhenIndexOutOfBounds()
+        {
+            //Arrange
+            Array<string> _ = new Array<string>(2)
+            {
+                //Act
+                [6] = "test"
+            };
+        }
     }
 }
