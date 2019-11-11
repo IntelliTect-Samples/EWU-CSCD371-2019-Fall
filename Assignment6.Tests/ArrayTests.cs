e0f7e2f4811b01e41227cd7c769f9c8187d5d81a
jake-mcg <55858256+jake-mcg@users.noreply.github.com>
2019-11-10 18:50:43 -0800

    Implemented ICollection<T>

--- Assignment6.Tests/ArrayTests.cs
+++ Assignment6.Tests/ArrayTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Assignment6.Tests
@@ -5,9 +6,91 @@ namespace Assignment6.Tests
     [TestClass]
     public class ArrayTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        public void Add_Success(int item)
         {
+            var sut = new Array<int>
+            {
+                1,
+                2,
+                3
+            };
+
+            sut.Add(item);
+
+            Assert.AreEqual(sut.Count, 4);
+            Assert.AreEqual(sut[3], item);
+        }
+
+        [TestMethod]
+        public void Clear_Success()
+        {
+            var sut = new Array<int>
+            {
+                1,
+                2,
+                3
+            };
+
+            sut.Clear();
+
+            Assert.AreEqual(0, sut.Count);
+
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        public void Contains_Success(int item)
+        {
+            var sut = new Array<int>
+            {
+                1,
+                2,
+                3
+            };
+
+            bool doesContain = sut.Contains(item);
+
+            Assert.IsTrue(doesContain);
+        }
+
+        [TestMethod]
+        public void CopyTo_Success()
+        {
+            var sut = new Array<int>
+            {
+                1,
+                2,
+                3
+            };
+            var target = new int[3];
+
+            sut.CopyTo(target, 0);
+
+            CollectionAssert.AreEqual(sut.ToArray(), target);
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        public void Remove_Success(int item)
+        {
+            var sut = new Array<int>
+            {
+                1,
+                2,
+                3
+            };
+
+            sut.Remove(item);
+
+            Assert.IsFalse(sut.Contains(item));
         }
     }
 }
--- Assignment6/Array.cs
+++ Assignment6/Array.cs
@@ -1,10 +1,70 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Assignment6
 {
-    public class Array<T>
+    public class Array<T> : ICollection<T>
     {
+        public int Count => _collection.Count;
+
+        public bool IsReadOnly => _collection.IsReadOnly;
+
+        private ICollection<T> _collection;
+
+        public Array()
+        {
+            _collection = new List<T>();
+        }
+
+        public Array(int size)
+        {
+            _collection = size < 0 ? new List<T>(size) : throw new ArgumentOutOfRangeException($"{nameof(size)} must be greater than zero.");
+        }
+
+        public T this[int index]
+        {
+
+            get
+            {
+                ValidateIndexIsInRange(index);
+                return _collection.AsEnumerable().ElementAt(index);
+            }
+            set
+            {
+                ValidateIndexIsInRange(index);
+                var temp = _collection.ToList();
+                temp[index] = value;
+                _collection = temp;
+            }
+        }
+
+        public void Add(T item)
+        {
+            _collection.Add(item);
+        }
+
+        public void Clear()
+        {
+            _collection.Clear();
+        }
+
+        public bool Contains(T item) => _collection.Contains(item);
+
+        public void CopyTo(T[] array, int arrayIndex) => _collection.CopyTo(array, arrayIndex);
         
+
+        public IEnumerator<T> GetEnumerator() => _collection.GetEnumerator();
+
+        public bool Remove(T item) => _collection.Remove(item);
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private void ValidateIndexIsInRange(int index)
+        {
+            if (index > _collection.Count || index < 0)
+                throw new IndexOutOfRangeException($"{nameof(index)} is out of range");
+        }
     }
 }
