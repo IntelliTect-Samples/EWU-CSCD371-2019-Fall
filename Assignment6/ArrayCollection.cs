e5b7eb287402cb5a082203f86cdf946247c6fcc9
Jacob Berger <jberger.1919@gmail.com>
2019-11-11 16:20:49 -0800

    Finished tests

---
+++ Assignment6/ArrayCollection.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assignment6
+{
+    public class ArrayCollection<T> : ICollection<T>
+    {
+        public int Capacity { get; }
+        public int Index { get; set; }
+        private ICollection<T> Collection { get; set; }
+        public ArrayCollection(int capacity)
+        {
+            if (!(capacity > 0)) {
+                throw new IndexOutOfRangeException("Width must be greater than 0");
+            }
+            Capacity = capacity;
+            Index = 0;
+            Collection = new List<T>(Capacity);
+        }
+
+        public int Count => Index;
+
+        public bool IsReadOnly => false;
+
+        public void Add(T item)
+        {
+            if (item is null)
+            {
+                throw new ArgumentNullException(nameof(item), "Passed in item is null");
+            }
+            if (!(Count == Capacity))
+            {
+                Collection.Add(item);
+                Index++;
+            }
+        }
+
+        public void Clear()
+        {
+            Collection.Clear();
+            Index = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            if (item is null)
+            {
+                throw new ArgumentNullException(nameof(item), "Passed in item is null");
+            }
+
+            return Collection.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array), "Array is null");
+            }
+
+            if (arrayIndex < 0 || arrayIndex > Capacity || array.Length < Capacity)
+            {
+                throw new IndexOutOfRangeException("Index out of bounds");
+            }
+
+            Collection.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            if (item is null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item is null");
+            }
+
+            if (Index == 0)
+            {
+                return false;
+            }
+
+            if (!Contains(item))
+            {
+                return false;
+            }
+
+            Collection.Remove(item);
+
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach(T item in Collection)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Collection.GetEnumerator();
+        }
+    }
+}
--- Assignment6Tests/ArrayTests.cs
+++ Assignment6Tests/ArrayTests.cs
@@ -10,58 +10,236 @@ namespace Assignment6.Tests
     public class ArrayTests
     {
         [TestMethod()]
-        public void ArrayTest()
+        public void ArrayTestGoodCapacity()
         {
             //Arrange
 
             //Act
-            Array<String> array = new Array<String>(10);
+            ArrayCollection<string> array = new ArrayCollection<string>(10);
 
             //Assert
             Assert.IsNotNull(array);
+        }
+
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(-1)]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void ArrayTestInvalidCapacityThrowsException(int capacity)
+        {
+            //Arrange
+
+            //Act
+            _ = new ArrayCollection<string>(capacity);
+
+            //Assert
         }
 
         [TestMethod()]
         public void AddTest()
         {
             //Arrange
-            Array<String> array = new Array<String>(10);
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
 
             //Act
             array.Add("Item");
 
             //Assert
-            Assert.AreEqual("Item", array.ToString());
+            Assert.IsTrue(array.Count == 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddTestNullItemThrowsException()
+        {
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
+
+            //Act
+            array.Add(null);
+
+            //Assert
         }
 
         [TestMethod()]
         public void ClearTest()
         {
-            Assert.Fail();
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
+            array.Add("Item");
+
+            //Act
+            array.Clear();
+
+            //Assert
+            Assert.IsTrue(array.Count == 0);
         }
 
         [TestMethod()]
         public void ContainsTest()
         {
-            Assert.Fail();
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
+            array.Add("Item");
+
+            //Act
+
+            //Assert
+            Assert.IsTrue(array.Contains("Item"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ContainsNullItem()
+        {
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
+
+            //Act
+            array.Contains(null);
         }
 
         [TestMethod()]
         public void CopyToTest()
         {
-            Assert.Fail();
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
+            array.Add("Item");
+            string[] destination = new string[1];
+
+            //Act
+            array.CopyTo(destination, 0);
+
+            //Assert
+            Assert.AreEqual(destination[0], "Item");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CopyToTestNullArrayThrowsException()
+        {
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
+            array.Add("Item");
+
+            //Act
+            array.CopyTo(null, 1);
+
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void CopyToTestInvalidIndexBelowRange()
+        {
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
+            array.Add("Item");
+            string[] destination = new string[1];
+
+            //Act
+            array.CopyTo(destination, -1);
+
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void CopyToTestInvalidIndexAboveRange()
+        {
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
+            array.Add("Item");
+            string[] destination = new string[1];
+
+            //Act
+            array.CopyTo(destination, 10);
+
+            //Assert
         }
 
         [TestMethod()]
         public void RemoveTest()
         {
-            Assert.Fail();
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(5);
+            array.Add("1");
+            array.Add("2");
+            array.Add("3");
+
+            //Act
+            array.Remove("2");
+
+            //Assert
+            Assert.IsFalse(array.Contains("2"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveTestNullItemThrowsException()
+        {
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
+
+            //Act
+            array.Remove(null);
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void RemoveTestEmptyArray()
+        {
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
+
+            //Act
+
+            //Assert
+            Assert.IsFalse(array.Remove("Should not exist"));
+        }
+
+        [TestMethod]
+        public void RemoveTestItemNotInArray()
+        {
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
+            array.Add("Item");
+
+            //Act
+
+            //Assert
+            Assert.IsFalse(array.Remove("Should not exist"));
         }
 
         [TestMethod()]
         public void GetEnumeratorTest()
         {
-            Assert.Fail();
+            //Arrange
+            ArrayCollection<string> array = new ArrayCollection<string>(5);
+            array.Add("1");
+            array.Add("2");
+            array.Add("3");
+            array.Add("4");
+            array.Add("5");
+
+            string[] plainArray = new string[] { "1", "2", "3", "4", "5" };
+
+            //Act
+            var enumerator = array.GetEnumerator();
+
+            //Assert
+            for (int ix = 0; ix < 5; ix++)
+            {
+                Assert.IsTrue(array.Contains(plainArray[ix]));
+            }
+        }
+
+        [TestMethod]
+        public void TestIsReadOnly()
+        {
+            ArrayCollection<string> array = new ArrayCollection<string>(1);
+            Assert.IsFalse(array.IsReadOnly);
         }
     }
 }
