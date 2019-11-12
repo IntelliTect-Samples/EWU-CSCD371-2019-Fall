2a61b007ca965ab68ee111dfa51a3160cd0eda29
Brett Henning <gamer.bah@live.com>
2019-11-11 23:21:01 -0800

    Assignment 6

---
+++ Assignment6.Tests/ArrayTests.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment6.Tests
+{
+
+    [TestClass]
+    public class ArrayTests
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_Given0_ThrowsException()
+        {
+            var unused = new ArrayCollection<int>(0);
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(5)]
+        [DataRow(10)]
+        public void ArrayOperator_OperatesCorrectly(int value)
+        {
+            var array = new ArrayCollection<int>(1);
+            array[0] = value;
+
+            Assert.AreEqual(value, array[0]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveItem_GivenNull_ThrowsException()
+        {
+            var array = new ArrayCollection<string>(2) {"a", "b"};
+            array.Remove(null);
+        }
+        
+        [TestMethod]
+        public void RemoveItem_RemovesCorrectItemFromArray()
+        {
+            var array = new ArrayCollection<string>(2) {"a", "b"};
+            array.Remove("a");
+
+            Assert.AreEqual(1, array.Count);
+            Assert.AreEqual("b", array[1]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Add_GivenNull_ThrowsException()
+        {
+            var array = new ArrayCollection<string>(1);
+            array.Add(null);
+        }
+        
+        [TestMethod]
+        public void Add_AddsToFirstEmptySpace()
+        {
+            var expected = new ArrayCollection<string>(5)
+            {
+                "c",
+                "a",
+                "b",
+                "d",
+                "e"
+            };
+            var array = new ArrayCollection<string>(5);
+            array[1] = "a";
+            array[3] = "b";
+            
+            array.Add("c");
+            array.Add("d");
+            array.Add("e");
+            
+            Assert.AreEqual(5, array.Count);
+            Assert.AreEqual(expected.ToString(), array.ToString());
+        }
+
+        [TestMethod]
+        public void CopyTo_CorrectlyCopiesArray()
+        {
+            var a = new ArrayCollection<string>(3) {"a", "b", "c"};
+            var b = new string[3];
+            a.CopyTo(b, 0);
+            
+            Assert.AreEqual(a.ToString(), b.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CopyTo_GivenNullArray_ThrowsException()
+        {
+            var a = new ArrayCollection<string>(3) {"a", "b", "c"};
+            // ReSharper disable once AssignNullToNotNullAttribute
+            a.CopyTo(null, 0);
+        }
+
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(3)]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CopyTo_GivenInvalidIndex_ThrowsException(int index)
+        {
+            var a = new ArrayCollection<string>(3) {"a", "b", "c"};
+            var b = new string[3];
+            a.CopyTo(b, index);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CopyTo_GivenDifferentArraySize_ThrowsException()
+        {
+            var a = new ArrayCollection<string>(3) {"a", "b", "c"};
+            var b = new string[2];
+            a.CopyTo(b, 0);
+        }
+
+        [TestMethod]
+        public void Contains_ReturnsCorrectValue()
+        {
+            var a = new ArrayCollection<string>(3) {"a", "b", "c"};
+            
+            Assert.IsTrue(a.Contains("a"));
+            Assert.IsFalse(a.Contains("d"));
+        }
+
+        [TestMethod]
+        public void Clear_CorrectlyClearsArray()
+        {
+            var a = new ArrayCollection<string>(3) {"a", "b", "c"};
+            a.Clear();
+            
+            Assert.AreEqual(3, a.Capacity);
+            Assert.AreEqual(0, a.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ReadOnlyException))]
+        public void WhileReadOnly_Operator_ThrowsException()
+        {
+            var a = new ArrayCollection<string>(3, true) {"a", "b", "c"};
+
+            a[0] = "d";
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ReadOnlyException))]
+        public void WhileReadOnly_Add_ThrowsException()
+        {
+            var a = new ArrayCollection<string>(3, true) {"a", "b"};
+
+            a.Add("c");
+        }
+
+    }
+
+}
--- Assignment6/Array.cs
+++ Assignment6/Array.cs
@@ -1,10 +1,129 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace Assignment6
 {
-    public class Array<T>
+
+    public class ArrayCollection<T> : ICollection<T>
     {
-        
+
+        private T[] _array;
+
+        public int  Capacity   { get; }
+        public bool IsReadOnly { get; }
+        public int  Count      { get; private set; }
+
+        public ArrayCollection(int capacity, bool isReadOnly = false)
+        {
+            if (capacity <= 0) throw new ArgumentException("must be > 0", nameof(capacity));
+
+            _array     = new T[capacity];
+            Capacity   = capacity;
+            IsReadOnly = isReadOnly;
+        }
+
+        public T this[int index]
+        {
+            get => Get(index);
+            set => Set(index, value);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _array.Cast<T>().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private T Get(int index)
+        {
+            if (index >= Capacity || index < 0) throw new IndexOutOfRangeException();
+
+            var item = _array[index];
+            if (item is null) throw new ArgumentException("No item at given index", nameof(index));
+            return item;
+        }
+
+        public void Add(T item)
+        {
+            if (Count == Capacity) throw new Exception("Array is full");
+
+            int index = 0;
+            while (_array[index] != null)
+            {
+                index++;
+            }
+
+            Set(index, item);
+        }
+
+        private void Set(int index, T item)
+        {
+            if (index >= Capacity || index < 0) throw new IndexOutOfRangeException();
+            if (item is null) throw new ArgumentNullException(nameof(index));
+            if (IsReadOnly) throw new ReadOnlyException("Array is marked as Read Only");
+
+            _array[index] = item;
+            Count++;
+        }
+
+        public void Clear()
+        {
+            _array = new T[Capacity];
+            Count  = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            foreach (var obj in _array)
+            {
+                if (obj.Equals(item)) return true;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int index)
+        {
+            if (array is null) throw new ArgumentNullException(nameof(array));
+            if (index >= Capacity || index < 0) throw new ArgumentOutOfRangeException(nameof(index));
+            if (Count > array.Length)
+                throw new ArgumentException("Current array contains more items than passed array can hold.",
+                                            nameof(array));
+            for (int x = 0; x < Capacity; x++)
+            {
+                if (_array[x] != null) array[x + index] = _array[x];
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+
+            for (int x = 0; x < Capacity; x++)
+            {
+                if (_array[x]!.Equals(item))
+                {
+                    _array[x] = default;
+                    Count--;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return _array.ToString();
+        }
+
     }
+
 }
