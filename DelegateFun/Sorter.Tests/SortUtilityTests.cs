6538f6c87a7b1c2bd1d42c8ea8b8032b0812de41
Doug Doner <ddoner02@gmail.com>
2019-11-14 15:45:27 -0800

    Adds first test for assignment

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -8,6 +8,15 @@ namespace Sorter.Tests
         [TestMethod]
         public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
         {
+            //Organize
+            int[] intArr = { 3, 4, 5, 2, 4, 2, 1};
+            SortUtility sortUtil = new SortUtility();
+
+            //Act
+            intArr = sortUtil.Sort(intArr, (x, y) => (x > y));
+
+            //Assert
+            Assert.AreEqual(1, intArr[0]);
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -4,7 +4,13 @@ namespace Sorter
 {
     public class SortUtility
     {
+        public delegate bool SortOrder(int first, int second);
         // Sort method should be implemented here
         // It should accept an int[] and a delegate you define that performs the actual comparison
+
+        public int[] Sort(int[] array, SortOrder sortOrder)
+        {
+            return new int[1];
+        }
     }
 }
