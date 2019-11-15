6c2cd73b62fe3d66e934f69d8d7e9dd2dd502baf
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2019-11-15 12:14:10 -0800

    Wrote first failing test as well as fixed compiling issue related to enabling nullable in Sorter.csproj

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,13 +1,30 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Sorter.Tests
 {
     [TestClass]
     public class SortUtilityTests
     {
+        private bool IsGreaterThan(int firstNumber, int secondNumber)
+        {
+            if (firstNumber > secondNumber)
+                return true;
+            return false;
+        }
+
         [TestMethod]
         public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
         {
+            //Arrange
+            SortUtility sortUtility = new SortUtility();
+            int[] unsortedArray = { 5, 2, 4, 1, 3 };
+            int[] sortedArray = { 1, 2, 3, 4, 5 };
+            //Act
+            sortUtility.Sort(unsortedArray, IsGreaterThan);
+            bool arrayIsSortedAscending = Enumerable.SequenceEqual(unsortedArray, sortedArray);
+            //Assert
+            Assert.IsTrue(arrayIsSortedAscending);
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -4,7 +4,13 @@ namespace Sorter
 {
     public class SortUtility
     {
+        public delegate bool SortingFuntion(int firstNumber, int secondNumber);
+
         // Sort method should be implemented here
         // It should accept an int[] and a delegate you define that performs the actual comparison
+        public void Sort(int[] unsortedArray, SortingFuntion sort)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
