10ddc19672e2f2dcdd579f1485efc44487136cbb
Joel <joelkalich1111@gmail.com>
2019-11-14 19:45:44 -0800

    Implemented SelectionSort method and the three required tests.

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,13 +1,62 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static Sorter.SortUtility;
 
-namespace Sorter.Tests
-{
+namespace Sorter.Tests {
     [TestClass]
-    public class SortUtilityTests
-    {
+    public class SortUtilityTests {
         [TestMethod]
-        public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
-        {
+        public void SelectionSort_ShouldSortAscending_UsingAnAnonymousMethod() {
+            //Arrange
+            int[] testArray = CreateRandomArray(200);
+
+            //Act
+            SelectionSort(testArray, delegate (int first, int second) { return first < second; });
+
+            //Assert
+            Assert.IsTrue(IsSorted(testArray));
+        }
+
+        [TestMethod]
+        public void SelectionSort_ShouldSortAscending_UsingLambdaExpression() {
+            //Arrange
+            int[] testArray = CreateRandomArray(200);
+
+            //Act
+            SelectionSort(testArray, (int first, int second) => first < second);
+
+            //Assert
+            Assert.IsTrue(IsSorted(testArray));
+        }
+
+        [TestMethod]
+        public void SelectionSort_ShouldSortAscending_UsingLambdaStatement() {
+            //Arrange
+            int[] testArray = CreateRandomArray(200);
+
+            //Act
+            SelectionSort(testArray, (int first, int second) => { return first < second; });
+
+            //Assert
+            Assert.IsTrue(IsSorted(testArray));
+        }
+
+        public int[] CreateRandomArray(int length) {
+            Random rand = new Random();
+            int[] randomArray = new int[length];
+            for (int i = 0; i < length; i++) {
+                randomArray[i] = rand.Next();
+            }
+            return randomArray;
+        }
+
+        public bool IsSorted(int[] a) {
+            for (int i = 1; i < a.Length; i++) {
+                if (a[i] < a[i - 1]) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 
-namespace Sorter
-{
-    public class SortUtility
-    {
-        // Sort method should be implemented here
-        // It should accept an int[] and a delegate you define that performs the actual comparison
+namespace Sorter {
+    public class SortUtility {
+        public delegate bool Comparer(int first, int second);
+        public static void SelectionSort(int[] array, Comparer comparer) {
+            for (int i = 0; i < array.Length - 1; i++) {
+                int minIndex = i;
+                for (int j = i + 1; j < array.Length; j++) {
+                    if (comparer(array[j], array[minIndex])) {
+                        minIndex = j;
+                    }
+                }
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
     }
 }
