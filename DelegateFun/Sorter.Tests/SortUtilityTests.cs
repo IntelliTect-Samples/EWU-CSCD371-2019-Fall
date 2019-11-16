2deb49a8ff41befdc875e200dfc51d7556031858
TatyanaHubbard <tatsihubbard@gmail.com>
2019-11-16 12:02:26 -0800

    Finished Tests

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
+using static Sorter.SortUtility;
 
 namespace Sorter.Tests
 {
@@ -9,19 +11,39 @@ namespace Sorter.Tests
         [TestMethod]
         public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
         {
+            int[] arr = { 9, 1, 3, 4, 2, 5, 7, 8, 6 };
+            int[] orderArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
+            SortUtility sortUtility = new SortUtility();
+            MyFunc myFunc = delegate (int first, int second) { return first < second; };
+
+            arr = sortUtility.Sort(arr, myFunc);
+
+            Assert.IsTrue(arr.SequenceEqual(orderArr));
         }
 
         [TestMethod]
         public void SortUtility_ShouldSortAscending_UsingALambdaStatement()
         {
+            int[] arr = { 9, 1, 3, 4, 2, 5, 7, 8, 6 };
+            int[] orderArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
+            SortUtility sortUtility = new SortUtility();
+            arr = sortUtility.Sort(arr, (int first, int second) => { return first < second; });
+
+            Assert.IsTrue(arr.SequenceEqual(orderArr));
         }
 
         [TestMethod]
         public void SortUtility_ShouldSortAscending_UsingALambdaExpression()
         {
+            int[] arr = { 9, 1, 3, 4, 2, 5, 7, 8, 6 };
+            int[] orderArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
+            SortUtility sortUtility = new SortUtility();
+            arr = sortUtility.Sort(arr, (int first, int second) => first < second);
+
+            Assert.IsTrue(arr.SequenceEqual(orderArr));
         }
 
         [TestMethod]
