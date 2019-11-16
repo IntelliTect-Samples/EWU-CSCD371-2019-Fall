54a51493300e2cb7071efcb2c3f427afd8749eba
jake-mcg <55858256+jake-mcg@users.noreply.github.com>
2019-11-16 10:06:24 -0800

    Implemented insertion sort and tests

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -5,22 +5,86 @@ namespace Sorter.Tests
     [TestClass]
     public class SortUtilityTests
     {
-        [TestMethod]
-        public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
+        [DataTestMethod]
+        [DataRow(new int[]{ 1, 2, 3 }, new int[] { 3, 2, 1 })]
+        [DataRow(new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new int[] { 6, 2, 5, 7, 3, 9, 8, 4, 10, 1 })]
+        [DataRow(new int[]{ 1, 123, 112343, 32987421, 432814732 }, new int[] { 432814732, 112343, 123, 1, 32987421 })]
+        public void InsertionSort_ShouldSortAscending_UsingAnAnonymousMethod(int[] expected, int[] actual)
         {
+            SortUtility sortUtility = new SortUtility();
+            Compare compare = delegate(int num1, int num2) { return num1 > num2; };
 
+            sortUtility.InsertionSort(actual, compare);
+
+            CollectionAssert.AreEqual(expected, actual);
         }
 
-        [TestMethod]
-        public void SortUtility_ShouldSortAscending_UsingALambdaExpression()
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 2, 3 }, new int[] { 3, 2, 1 })]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new int[] { 6, 2, 5, 7, 3, 9, 8, 4, 10, 1 })]
+        [DataRow(new int[] { 1, 123, 112343, 32987421, 432814732 }, new int[] { 432814732, 112343, 123, 1, 32987421 })]
+        public void InsertionSort_ShouldSortAscending_UsingALambdaExpression(int[] expected, int[] actual)
         {
+            SortUtility sortUtility = new SortUtility();
+            Compare compare = (num1, num2) => num1 > num2;
 
+            sortUtility.InsertionSort(actual, compare);
+
+            CollectionAssert.AreEqual(expected, actual);
         }
 
-        [TestMethod]
-        public void SortUtility_ShouldSortAscending_UsingALambdaStatement()
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 2, 3 }, new int[] { 3, 2, 1 })]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new int[] { 6, 2, 5, 7, 3, 9, 8, 4, 10, 1 })]
+        [DataRow(new int[] { 1, 123, 112343, 32987421, 432814732 }, new int[] { 432814732, 112343, 123, 1, 32987421 })]
+        public void InsertionSort_ShouldSortAscending_UsingALambdaStatement(int[] expected, int[] actual)
         {
+            SortUtility sortUtility = new SortUtility();
 
+            sortUtility.InsertionSort(actual, (num1, num2) => num1 > num2);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        [DataRow(new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, new int[] { 6, 2, 5, 7, 3, 9, 8, 4, 10, 1 })]
+        [DataRow(new int[] { 432814732, 32987421, 112343, 123, 1 }, new int[] { 432814732, 112343, 123, 1, 32987421 })]
+        public void InsertionSort_ShouldSortDescending_UsingAnAnonymousMethod(int[] expected, int[] actual)
+        {
+            SortUtility sortUtility = new SortUtility();
+            Compare compare = delegate (int num1, int num2) { return num1 < num2; };
+
+            sortUtility.InsertionSort(actual, compare);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        [DataRow(new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, new int[] { 6, 2, 5, 7, 3, 9, 8, 4, 10, 1 })]
+        [DataRow(new int[] { 432814732, 32987421, 112343, 123, 1 }, new int[] { 432814732, 112343, 123, 1, 32987421 })]
+        public void InsertionSort_ShouldSortDescending_UsingALambdaExpression(int[] expected, int[] actual)
+        {
+            SortUtility sortUtility = new SortUtility();
+            Compare compare = (num1, num2) => num1 < num2;
+
+            sortUtility.InsertionSort(actual, compare);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        [DataRow(new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, new int[] { 6, 2, 5, 7, 3, 9, 8, 4, 10, 1 })]
+        [DataRow(new int[] { 432814732, 32987421, 112343, 123, 1 }, new int[] { 432814732, 112343, 123, 1, 32987421 })]
+        public void InsertionSort_ShouldSortDescending_UsingALambdaStatement(int[] expected, int[] actual)
+        {
+            SortUtility sortUtility = new SortUtility();
+
+            sortUtility.InsertionSort(actual, (num1, num2) => num1 < num2);
+
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -2,9 +2,27 @@
 
 namespace Sorter
 {
+    public delegate bool Compare(int first, int second);
+
     public class SortUtility
     {
         // Sort method should be implemented here
         // It should accept an int[] and a delegate you define that performs the actual comparison
+
+        public void InsertionSort(int[] array, Compare compare)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (compare(array[j - 1], array[j]))
+                    {
+                        int temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
     }
 }
