59de21bf6623e105947fabf5a10edf2a45d0e577
CDobbins1 <cdobsc01@hotmail.com>
2019-11-17 11:24:39 -0800

    Should be done.

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Sorter.Tests
 {
@@ -8,6 +9,48 @@ namespace Sorter.Tests
         [TestMethod]
         public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
         {
+            int[] array = { 5, 4, 21, 64, 7, 28, 0, 84, 123, 42};
+            int[] expectedResult = { 0, 4, 5, 7, 21, 28, 42, 64, 84, 123};
+
+            SortUtility.Sort(array, delegate(int first, int second) 
+            {
+                return first < second;
+            });
+
+            CollectionAssert.AreEqual(expectedResult, array);
+        }
+
+        [TestMethod]
+        public void SortUtility_ShouldSortDescending_UsingLambdaExpression()
+        {
+            int[] array = { 5, 4, 21, 64, 7, 28, 0, 84, 123, 42 };
+            int[] expectedResult = { 123, 84, 64, 42, 28, 21, 7, 5, 4, 0 };
+
+            SortUtility.Sort(array, (first, second) => first > second);
+
+            CollectionAssert.AreEqual(expectedResult, array);
+        }
+
+        [TestMethod]
+        public void SortUtility_ShouldSortAlphabeticalAscending_UsingLambdaStatement()
+        {
+            int[] array = { 5, 4, 21, 64, 7, 28, 0, 84, 123, 42 };
+            int[] expectedResult = { 0, 123, 21, 28, 4, 42, 5, 64, 7, 84 };
+
+            SortUtility.Sort(array, (first, second) =>
+            {
+                int comparison = second.ToString().CompareTo(first.ToString());
+                return comparison > 0;
+            });
+
+            CollectionAssert.AreEqual(expectedResult, array);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortUtility_UsingNullArray_ThrowsException()
+        {
+            SortUtility.Sort(null!, (first, second) => first < second);
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -2,9 +2,27 @@
 
 namespace Sorter
 {
-    public class SortUtility
+    public static class SortUtility
     {
-        // Sort method should be implemented here
-        // It should accept an int[] and a delegate you define that performs the actual comparison
+        public delegate bool Comparer(int first, int second);
+        public static void Sort(int[] array, Comparer compare)
+        {
+            if (array is null) throw new ArgumentNullException(nameof(array));
+
+            if (array.Length <= 1) return;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && compare(key, array[j]))
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[j + 1] = key;
+            }
+        }
     }
 }
