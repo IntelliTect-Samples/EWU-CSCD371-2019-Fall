74eb11f879d319bfca7ef4634c2bd046118cdaee
Asher Mancinelli <ashermancinelli@gmail.com>
2019-11-18 10:28:29 -0800

    adding first tests
    Added seperate methods for tests to be implemented, as well
    as both tests for the delegate methods

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,12 +1,86 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Sorter;
 
 namespace Sorter.Tests
 {
     [TestClass]
     public class SortUtilityTests
     {
-        [TestMethod]
-        public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
+        [DataTestMethod]
+        [DataRow(new int[] {3, 5, 1, 4, 2})]
+        [DataRow(new int[] {5, 4, 3, 2, 1})]
+        [DataRow(new int[] {1, 2, 3, 4, 5})]
+        public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod(int[] arr)
+        {
+            var sut = new SortUtility();
+
+            Key key = delegate (int a, int b)
+            {
+                return a > b;
+            };
+
+            int[] expected = arr;
+            
+            Array.Sort(expected);
+
+            sut.QuickSort(arr, key);
+
+            Assert.AreEqual(expected, arr);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] {3, 5, 1, 4, 2})]
+        [DataRow(new int[] {5, 4, 3, 2, 1})]
+        [DataRow(new int[] {1, 2, 3, 4, 5})]
+        public void SortUtility_ShouldSortDescending_UsingAnAnonymousMethod(int[] arr)
+        {
+            var sut = new SortUtility();
+
+            Key key = delegate (int a, int b)
+            {
+                return a < b;
+            };
+
+            int[] expected = arr;
+            
+            Array.Sort(expected);
+            Array.Reverse(expected);
+
+            sut.QuickSort(arr, key);
+
+            Assert.AreEqual(expected, arr);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] {3, 5, 1, 4, 2})]
+        [DataRow(new int[] {5, 4, 3, 2, 1})]
+        [DataRow(new int[] {1, 2, 3, 4, 5})]
+        public void SortUtility_ShouldSortAscending_UsingLambdaExpression(int[] arr)
+        {
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] {3, 5, 1, 4, 2})]
+        [DataRow(new int[] {5, 4, 3, 2, 1})]
+        [DataRow(new int[] {1, 2, 3, 4, 5})]
+        public void SortUtility_ShouldSortDescending_UsingLambdaExpression(int[] arr)
+        {
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] {3, 5, 1, 4, 2})]
+        [DataRow(new int[] {5, 4, 3, 2, 1})]
+        [DataRow(new int[] {1, 2, 3, 4, 5})]
+        public void SortUtility_ShouldSortAscending_UsingLambdaStatement(int[] arr)
+        {
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] {3, 5, 1, 4, 2})]
+        [DataRow(new int[] {5, 4, 3, 2, 1})]
+        [DataRow(new int[] {1, 2, 3, 4, 5})]
+        public void SortUtility_ShouldSortDescending_UsingLambdaStatement(int[] arr)
         {
         }
     }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -2,42 +2,67 @@
 
 namespace Sorter
 {
+    public delegate bool Key(int first, int second);
+
     public class SortUtility
     {
-        public delegate bool Key(int first, int second);
+        private Key? _Key; 
 
-        private void QuickSort(int[] arr, Key key, int l, int r)
+        public static void Main(string[] args)
+        {
+            var sorter = new SortUtility();
+
+            var arr = new int[]
+            {
+                3, 4, 2, 5, 1
+            };
+
+            Key k = (int a, int b) => a < b;
+
+            sorter.QuickSort(arr, k);
+            foreach (int i in arr)
+                Console.WriteLine($"Value: {i}");
+        }
+
+        private void QuickSort(int[] arr, int l, int r)
         {
             if (l < r)
             {
                 int piv = Partition(arr, l, r);
 
-                if (piv > 1) QuickSort(int[] arr, l, piv - 1);
-                if (piv + 1 < r) QuickSort(int[] arr, piv + 1, r);
+                QuickSort(arr, l, piv - 1);
+                QuickSort(arr, piv + 1, r);
             }
         }
 
-        public void QuickSort(int[] arr, Key key) =>
-            QuickSort(arr, 0, arr.Length-1);
-
-        private void Partition(int[] arr, int l, int r)
+        public void QuickSort(int[] arr, Key? key)
         {
-            int piv = arr[l];
-            while (true)
-            {
-                while (arr[l] < piv) l++;
-                while (arr[r] > piv) r++;
-                if (l < r)
-                {
-                    if (arr[l] == arr[r]) return r;
+            if (key is null)
+                throw new ArgumentNullException(nameof(key));
+            _Key = key;
+            QuickSort(arr, 0, arr.Length-1);
+        }
 
-                    int temp = arr[l];
-                    arr[l] = arr[r];
-                    arr[r] = temp;
+        private int Partition(int[] arr, int l, int r)
+        {
+            int piv = arr[r],
+                i = l - 1,
+                temp;
+
+            for (int j = l; j < r; j++)
+            {
+                if (_Key(arr[j], piv))
+                {
+                    temp = arr[++i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
-                else
-                    return r;
             }
+
+            temp = arr[i+1];
+            arr[i+1] = arr[r];
+            arr[r] = temp;
+            return i + 1;
         }
     }
 }
