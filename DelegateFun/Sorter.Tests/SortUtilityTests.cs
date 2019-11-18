4cc4b579cf16f8665c43c0f09e160c0ab094b6b3
Jerett Latimer <jerettlatimer@yahoo.com>
2019-11-17 19:10:31 -0800

    Initial Commit

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Sorter.Tests
 {
@@ -8,6 +9,51 @@ namespace Sorter.Tests
         [TestMethod]
         public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
         {
+            // Arrange
+            int[] test = { 4, 2, 3, 1 };
+            int[] expected = { 1, 2, 3, 4 };
+
+            // Act
+            SortUtility.SelectionSort(test, delegate (int first, int second)
+            {
+                return first < second;
+            });
+
+            // Assert
+            CollectionAssert.AreEqual(test, expected);
+        }
+
+        [TestMethod]
+        public void SortUtility_SortDescending_UsingLambdaExpression()
+        {
+            // Arrange
+            int[] test = { 1, 4, 2, 3 };
+            int[] expected = { 4, 3, 2, 1 };
+
+            // Act
+            SortUtility.SelectionSort(test, (int first, int second) => first > second);
+
+            // Assert
+            CollectionAssert.AreEqual(test, expected);
+
+        }
+
+        [TestMethod]
+        public void SortUtility_SortDescendingAlphabetical_UsingLambdaStatement()
+        {
+            // Arrange
+            int[] test = { 1, 4, 2, 3 };
+            int[] expected = { 4, 3, 2, 1 };
+
+            // Act
+            SortUtility.SelectionSort(test, (int first, int second) =>
+            {
+                return first.ToString().CompareTo(second.ToString()) > 0;
+            });
+
+            // Assert
+            CollectionAssert.AreEqual(test, expected);
+
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -4,7 +4,29 @@ namespace Sorter
 {
     public class SortUtility
     {
+        public delegate bool SortFunc(int first, int second);
+
         // Sort method should be implemented here
         // It should accept an int[] and a delegate you define that performs the actual comparison
+
+        public static int[] SelectionSort(int[] items, SortFunc compare)
+        {
+            int smallest;
+            for(int i = 0; i < items.Length; i++)
+            {
+                smallest = i;
+                for(int j = i + 1; j < items.Length; j++)
+                {
+                    if(compare(items[j], items[smallest]))
+                    {
+                        smallest = j;
+                    }
+                }
+                int temp = items[i];
+                items[i] = items[smallest];
+                items[smallest] = temp;
+            }
+            return items;
+        }
     }
 }
