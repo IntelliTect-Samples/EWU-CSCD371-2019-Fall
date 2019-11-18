39e1055949a655cf36e560089a51a6b7ce482e23
dbosco051394 <abosco@eagles.ewu.edu>
2019-11-17 20:44:56 -0800

    Finished code. Ready for review.

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Sorter.Tests
 {
@@ -8,6 +9,62 @@ namespace Sorter.Tests
         [TestMethod]
         public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
         {
+            int[] arr = { 3, 4, 1, 8, 10, 2, 100, 33 };
+            int[] expectedResults = { 1, 2, 3, 4, 8, 10, 33, 100 };
+            SortUtility.ComparisonDel del = delegate(int num1, int num2)
+            {
+                return num1 > num2;
+            };
+            SortUtility.InsertionSort(arr, del);
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Assert.AreEqual(arr[i], expectedResults[i]);
+            }
+        }
+
+        [TestMethod]
+        public void SortUtility_ShouldSortDescending_UsingLambdaExpression()
+        {
+            int[] arr = { 3, 4, 1, 8, 10, 2, 100, 33 };
+            int[] expectedResults = {100, 33, 10, 8, 4, 3, 2, 1};
+            SortUtility.InsertionSort(arr, (num1, num2) =>
+            {
+                return num1 < num2;
+            });
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Assert.AreEqual(arr[i], expectedResults[i]);
+            }
+        }
+
+        [TestMethod]
+        public void SortUtility_ShouldSortOddsFirst_NonAscending_UsingLambdaStatement()
+        {
+            int[] arr = { 3, 4, 1, 8, 10, 2, 100, 33 };
+
+            SortUtility.InsertionSort(arr, (num1, num2) => num2 % 2 != 0);
+
+            //Expecting arr to be sorted as such: { odd, odd, odd, odd, even, even, even, even}
+            //The sort does not also do subordering for each group ascending nor descending, just separates them
+            bool isEvenFlag = false;
+            foreach(int x in arr)
+            {
+                bool check = x % 2 == 0;
+                if (check) isEvenFlag = true;
+                if(isEvenFlag)
+                {
+                    Assert.IsTrue(check);
+                }
+                else
+                {
+                    Assert.IsFalse(check);
+                }
+            }
+        }
+
+        public bool CompareNum(int num1, int num2)
+        {
+            return num1 > num2;
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -6,5 +6,25 @@ namespace Sorter
     {
         // Sort method should be implemented here
         // It should accept an int[] and a delegate you define that performs the actual comparison
+
+        //Will return true if num1 is greater than num2
+        public delegate bool ComparisonDel(int num1, int num2);
+        public static void InsertionSort(int[] arr, ComparisonDel comparison)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int num2 = arr[i];
+                int j = i - 1;
+
+                while(j>=0 && comparison(arr[j], num2))
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = num2;
+            }
+        }
+
     }
 }
