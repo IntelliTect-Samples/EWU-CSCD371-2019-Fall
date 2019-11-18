81e262ca6cb3dbc5130515f585b54b45a0e6189c
Asher Mancinelli <ashermancinelli@gmail.com>
2019-11-18 08:48:54 -0800

    init sorting methods

--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -4,7 +4,40 @@ namespace Sorter
 {
     public class SortUtility
     {
-        // Sort method should be implemented here
-        // It should accept an int[] and a delegate you define that performs the actual comparison
+        public delegate bool Key(int first, int second);
+
+        private void QuickSort(int[] arr, Key key, int l, int r)
+        {
+            if (l < r)
+            {
+                int piv = Partition(arr, l, r);
+
+                if (piv > 1) QuickSort(int[] arr, l, piv - 1);
+                if (piv + 1 < r) QuickSort(int[] arr, piv + 1, r);
+            }
+        }
+
+        public void QuickSort(int[] arr, Key key) =>
+            QuickSort(arr, 0, arr.Length-1);
+
+        private void Partition(int[] arr, int l, int r)
+        {
+            int piv = arr[l];
+            while (true)
+            {
+                while (arr[l] < piv) l++;
+                while (arr[r] > piv) r++;
+                if (l < r)
+                {
+                    if (arr[l] == arr[r]) return r;
+
+                    int temp = arr[l];
+                    arr[l] = arr[r];
+                    arr[r] = temp;
+                }
+                else
+                    return r;
+            }
+        }
     }
 }
