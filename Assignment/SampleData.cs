83dbce90646b8a540817aa4712f57edec9214dea
Jacob Berger <jberger.1919@gmail.com>
2019-11-21 13:48:51 -0800

    WIP
    No idea if this works

--- Assignment/SampleData.cs
+++ Assignment/SampleData.cs
@@ -1,16 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Assignment
 {
+
+    /*1. Implement the `ISampleData.CsvRows` property, loading the data from the `People.csv` file and returning each line as a single string.
+
+- Change the "Copy to" property on People.csv to "Copy if newer" so that the file is deployed along with your test project.
+- Using LINQ, skip the first row in the `People.csv`.
+- Be sure to appropriately handle resource (`IDisposable`) items correctly if applicable (and it may not be depending on how you implement it).
+
+    */
     public class SampleData : ISampleData
     {
+
+        private string? _FileName;
+
+        public SampleData(string? fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            } else
+            {
+                _FileName = fileName;
+            }
+        }
+
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+        public IEnumerable<string> CsvRows
+        {
+            get
+            {
+                using StreamReader streamReader = new StreamReader(_FileName);
+                streamReader.ReadLine();
+                yield return streamReader.ReadLine();
+            }
+        }
+
+        /*
+         * Implement `IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()` to return a **sorted**, **unique** list of states.
+
+- Use `ISampleData.CsvRows` for your data source.
+- Don't forget the list should be unique.
+- Sort the list by State, City, Zip.
+- Include a test that leverages a hard coded list of Spokane based addresses.
+- Include a test that uses LINQ to verify the data is sorted correctly (do not use a hard coded list).
+
+         * 
+         */
 
         // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => throw new NotImplementedException();
+        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+        {
+
+            var sorted = CsvRows.Select(line => new
+            {
+                State = line.Split(',')[6],
+                City = line.Split(',')[5],
+                Zip = line.Split(',')[7],
+                Line = line
+            }).Distinct().OrderBy(x => x.State).ThenBy(x => x.City).ThenBy(x => x.Zip);
+
+            IEnumerable<string> result = (IEnumerable<string>)sorted;
+            return result;
+        }
 
         // 3.
         public string GetAggregateSortedListOfStatesUsingCsvRows()
