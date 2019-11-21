b43b91349193566db1a9c718ce4b52d0f3baa40e
Jacob Berger <jberger.1919@gmail.com>
2019-11-21 13:48:51 -0800

    WIP
    No idea if this works

---
+++ AssignmentTests/SampleDataTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment.Tests
+{
+    [TestClass()]
+    public class SampleDataTests
+    {
+        [TestMethod()]
+        public void SampleDataTest()
+        {
+            SampleData data = new SampleData("People.csv");
+
+            Console.WriteLine(data.CsvRows);
+        }
+    }
+}
