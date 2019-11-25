76434f91452f13b9a60e1d002f0b4d4d6df87dd4
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-11-25 12:20:01 -0800

    Update classes

--- Assignment/Address.cs
+++ Assignment/Address.cs
@@ -1,10 +1,20 @@
-﻿namespace Assignment
+﻿using System;
+
+namespace Assignment
 {
     public class Address : IAddress
     {
-        public string StreetAddress { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
+        public string StreetAddress { get; }
+        public string City { get; }
+        public string State { get; }
+        public string Zip { get; }
+
+        public Address(string streetAddress, string city, string state, string zip)
+        {
+            StreetAddress = streetAddress ?? throw new ArgumentNullException(nameof(streetAddress));
+            City = city ?? throw new ArgumentNullException(nameof(city));
+            State = state ?? throw new ArgumentNullException(nameof(state));
+            Zip = zip ?? throw new ArgumentNullException(nameof(zip));
+        }
     }
 }
--- Assignment/Person.cs
+++ Assignment/Person.cs
@@ -1,14 +1,22 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Assignment
 {
     public class Person : IPerson
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public IAddress Address { get;set; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public IAddress Address { get; }
+        public string EmailAddress { get; }
 
-        public string EmailAddress { get; set; }
+        public Person(string firstName, string lastName, string emailAddress, IAddress address)
+        {
+            FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+            LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+            EmailAddress = emailAddress ?? throw new ArgumentNullException(nameof(emailAddress));
+            Address = address ?? throw new ArgumentNullException(nameof(address));
+        }
     }
 }
