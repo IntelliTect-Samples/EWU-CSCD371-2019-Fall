c9bb870560a1d1a93e863c7a83474c0332751da3
skozubenko1 <55997642+skozubenko1@users.noreply.github.com>
2019-11-26 11:55:51 -0800

    worked on tests

--- Assignment.Tests/Tests.cs
+++ Assignment.Tests/Tests.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+
 
 
 namespace Assignment.Tests
@@ -12,14 +14,37 @@ namespace Assignment.Tests
     public class Tests
     {
         [TestMethod]
-        public void TestPersonEquals()
+        public void Test_FilterByEmailAddress()
         {
-           
+            //not yet fully implemented..
+            SampleData data = new SampleData();
+
+            string csvRows = "1, Priscilla, Jenyns, pjenyns0@state.gov,7884 Corry Wa y, Helena, MT,70577\n" +
+                "2,Karin,Joder,kjoder1 @quantcast.com,03594 Florence Park, Tampa, FL,71961\n" +
+                "3,Chadd,Stennine,cstennine2 @wired.com,94148 Kings Terrace, Long Beach,CA,59721";
+
         }
 
-        
+        [TestMethod]
+        public void GetUniqueSortedListOfStates_Linq_ReturnsCorrectlySortedList()
+        {
+            SampleData _Data = new SampleData();
+
+            string expected_result = "ALAZCADCFLGAINKSLAMDMNMOMTNCNENHNVNYORPASCTNTXUTVAWAWV";
+
+            IEnumerable<string> states = _Data.GetUniqueSortedListOfStatesGivenCsvRows();
+
+            string my_result = "";
+
+            foreach(var line in states)
+            {
+                my_result += line;
+            }
+
+            //Console.Write(my_result);
+            Assert.AreEqual(expected_result, my_result);
+
+        }
     }
-
-
 }
 
