55c6a037294663d30f076607d4b62eb30397a08e
skozubenko1 <skozubenko1@eagles.ewu.edu>
2019-11-26 00:44:12 -0800

    Implemented methods

---
+++ Assignment.Tests/Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace Assignment.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestPersonEquals()
+        {
+           
+        }
+
+        
+    }
+
+
+}
+
---
+++ Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+
+        }
+    }
+}
