0c788293fc66796ca64b857aabf8ae514f242148
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-12-05 16:10:09 -0800

    Add MainWindow viewmodel

---
+++ ShoppingList/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+
+namespace ShoppingList
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Item> Items { get; } = new ObservableCollection<Item>();
+
+        private Item? _SelectedItem;
+        public Item? SelectedItem
+        {
+            get => _SelectedItem;
+            set => Set(ref _SelectedItem, value);
+        }
+
+        public RelayCommand AddItem { get; }
+
+        private void OnAddItem()
+        {
+            var newItem = new Item();
+            Items.Add(newItem);
+            SelectedItem = newItem;
+        }
+
+        public MainWindowViewModel()
+        {
+            AddItem = new RelayCommand(OnAddItem);
+        }
+    }
+}
