7f2323275d514327981f97ac8fed0b5d6b88cf48
Doug Doner <ddoner02@gmail.com>
2019-12-05 18:32:57 -0800

    Initial Project setup and stubbing out files

---
+++ ShoppingList/ShoppingList/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ShoppingList
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public ObservableCollection<ShoppingItem> ShoppingList { get; } = new ObservableCollection<ShoppingItem>();
+    }
+}
---
+++ ShoppingList/ShoppingList/ShoppingItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingList
+{
+    public class ShoppingItem
+    {
+        public string ItemName { get; set; }
+
+        public ShoppingItem(string itemName)
+        {
+            this.ItemName = itemName;
+        }
+    }
+}
