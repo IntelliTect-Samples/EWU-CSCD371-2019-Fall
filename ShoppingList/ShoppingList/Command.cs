e79c867f5d2ac4778a263914e8a3f7eebe078326
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2019-12-06 11:32:49 -0800

    Assighnment far from finished, just getting pull request out of the way

--- ShoppingList/ShoppingList/Command.cs
+++ ShoppingList/ShoppingList/Command.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
-
+//comment put here to enable pull request, delete later
 namespace ShoppingList
 {
     public class Command : ICommand
