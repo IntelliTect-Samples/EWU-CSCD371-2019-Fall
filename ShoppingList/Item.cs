a891579ba3619fee238a4ee7a38e38e8bb2d1acc
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-12-06 20:05:42 -0800

    Implement Item

--- ShoppingList/Item.cs
+++ ShoppingList/Item.cs
@@ -2,5 +2,12 @@
 {
     public class Item
     {
+        public string Name { get; set; } = "New Item";
+
+        public override bool Equals(object? obj) =>
+            obj is Item item && Name == item.Name;
+
+        public override int GetHashCode() =>
+            Name.GetHashCode();
     }
 }
