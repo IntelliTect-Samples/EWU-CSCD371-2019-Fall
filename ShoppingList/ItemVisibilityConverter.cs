d4675905951ce94a12ae382526549be3097db231
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2019-12-06 20:06:04 -0800

    Add ItemVisibilityConverter

---
+++ ShoppingList/ItemVisibilityConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ShoppingList
+{
+    public class ItemVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type? targetType = null, object? parameter = null, CultureInfo? culture = null) =>
+            value is Item ? "Visible" : "Hidden";
+
+        public object ConvertBack(object value, Type? targetType = null, object? parameter = null, CultureInfo? culture = null) =>
+            throw new InvalidOperationException();
+    }
+}
