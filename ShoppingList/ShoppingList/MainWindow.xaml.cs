1da54600b43cfdd53b032414b63846d40ca4bb9e
TatyanaHubbard <tatsihubbard@gmail.com>
2019-12-08 13:24:56 -0800

    Hopefully Fixed a few things

--- ShoppingList/ShoppingList/MainWindow.xaml.cs
+++ ShoppingList/ShoppingList/MainWindow.xaml.cs
@@ -1,15 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Windows;
+﻿using System.Windows;
 
 namespace ShoppingList
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
