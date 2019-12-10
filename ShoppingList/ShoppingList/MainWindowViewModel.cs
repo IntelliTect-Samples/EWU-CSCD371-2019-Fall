f4c147e2c9cb1fa384f4ba66190c75161202871e
TatyanaHubbard <tatsihubbard@gmail.com>
2019-12-10 10:04:03 -0800

    Add now works and added Visibility Converter

--- ShoppingList/ShoppingList/MainWindowViewModel.cs
+++ ShoppingList/ShoppingList/MainWindowViewModel.cs
@@ -1,64 +1,52 @@
-﻿using GalaSoft.MvvmLight.Command;
-using System.Collections.Generic;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace ShoppingList
 {
-    public class MainWindowViewModel: INotifyPropertyChanged
+    public class MainWindowViewModel : ViewModelBase
     {
-        private ObservableCollection<ShopItem> MyShoppingList { get; } = new ObservableCollection<ShopItem>();
+        public ObservableCollection<ShopItem> MyShoppingList { get; } = new ObservableCollection<ShopItem>();
 
         private ShopItem _SelectedShopItem;
         public ShopItem SelectedShopItem
         {
             get => _SelectedShopItem;
-            set => SetProperty(ref _SelectedShopItem, value);
+            set => Set(ref _SelectedShopItem, value);
         }
 
-        private string _NewName = "";
+        private string _NewName;
         public string NewName
         {
             get => _NewName;
-            set => SetProperty(ref _NewName, value);
+            set => Set(ref _NewName, value);
         }
 
         public RelayCommand AddItem { get; }
 
         public MainWindowViewModel()
         {
-            MyShoppingList.Add(new ShopItem("Banana"));
-            MyShoppingList.Add(new ShopItem("Apple"));
-            MyShoppingList.Add(new ShopItem("Peach"));
-            _SelectedShopItem = MyShoppingList.First();
-
             AddItem = new RelayCommand(OnAddItem);
+            _NewName = "";
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(field, value))
-            {
-                field = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-
 
         private void OnAddItem()
         {
             if (!string.IsNullOrWhiteSpace(NewName))
             {
-                var newItem = new ShopItem(NewName);
+                ShopItem newItem = new ShopItem(NewName);
                 MyShoppingList.Add(newItem);
                 SelectedShopItem = newItem;
                 NewName = "";
             }
         }
 
+        private void OnEditItem()
+        {
+            if(!(SelectedShopItem is null))
+            {
+
+            }
+        }
     }
 }
