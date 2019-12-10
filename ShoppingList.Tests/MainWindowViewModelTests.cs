1771985c7ce1f877a23ffc1474c0de6d166238b4
Jacob Berger <jberger.1919@gmail.com>
2019-12-10 02:26:31 -0800

    Finished tests

---
+++ ShoppingList.Tests/MainWindowViewModelTests.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingList;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ShoppingList.Tests
+{
+    [TestClass()]
+    public class MainWindowViewModelTests
+    {
+        [TestMethod()]
+        public void ShoppingListAddItem()
+        {
+            //Arrange
+            ObservableCollection<Item> list = new ObservableCollection<Item>();
+            Item item = new Item("Test Item");
+            int beforeSize = list.Count;
+
+            //Act
+            list.Add(item);
+
+            //Assert
+            Assert.AreNotEqual(beforeSize, list.Count);
+            Assert.IsTrue(list.Contains(item));
+        }
+
+        [TestMethod]
+        public void ShoppingListAddNullItem()
+        {
+            //Arrange
+            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
+            string itemName = null!;
+            mainWindowViewModel.TextToAdd = itemName;
+
+            //Act
+            mainWindowViewModel.AddItemCommand.Execute(itemName);
+
+            //Assert
+            Assert.AreEqual(mainWindowViewModel.ShoppingList.Count, 0);
+        }
+
+        [TestMethod]
+        public void AddItemEmptyText()
+        {
+            //Arrange
+            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
+            string itemName = "";
+            mainWindowViewModel.TextToAdd = itemName;
+
+            //Act
+            mainWindowViewModel.AddItemCommand.Execute(itemName);
+
+            //Assert
+            Assert.AreEqual(mainWindowViewModel.ShoppingList.Count, 0);
+        }
+
+        [TestMethod]
+        public void AddItemGoodName()
+        {
+            //Arrange
+            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
+            string itemName = "Test Item";
+            mainWindowViewModel.TextToAdd = itemName;
+
+            //Act
+            mainWindowViewModel.AddItemCommand.Execute(itemName);
+
+            //Assert
+            Assert.AreEqual(mainWindowViewModel.ShoppingList.Count, 1);
+        }
+
+        [TestMethod]
+        public void DeleteItemNotNull()
+        {
+            //Arrange
+            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
+            Item item = new Item("Test Item");
+            mainWindowViewModel.ShoppingList.Add(item);
+            mainWindowViewModel.SelectedItem = item;
+
+            //Act
+            mainWindowViewModel.DeleteItemCommand.Execute(mainWindowViewModel.SelectedItem);
+
+            //Assert
+            Assert.AreEqual(mainWindowViewModel.ShoppingList.Count, 0);
+        }
+
+        [TestMethod]
+        public void DeleteItemNull()
+        {
+            //Arrange
+            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
+            Item item = new Item("Test Item");
+            mainWindowViewModel.ShoppingList.Add(item);
+            mainWindowViewModel.SelectedItem = null;
+
+            //Act
+            mainWindowViewModel.DeleteItemCommand.Execute(mainWindowViewModel.SelectedItem);
+
+            //Assert
+            Assert.AreEqual(mainWindowViewModel.ShoppingList.Count, 1);
+        }
+    }
+}
