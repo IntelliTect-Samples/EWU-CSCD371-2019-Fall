60a73e52524bec5d10382535994509ef0d43be61
rowland17 <rowland17@up.edu>
2020-07-30 11:32:19 -0700

    Create a ConsoleLogger class to log to console.
    Write unit test that routes console output to file for verification.

---
+++ Logger/ConsoleLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger
+{
+    public class ConsoleLogger : BaseLogger
+    {
+        public override void Log(LogLevel logLevel, string message)
+        {
+            string loggingLevel;
+
+            switch (logLevel)
+            {
+                case LogLevel.Information:
+                    loggingLevel = "Information";
+                    break;
+                case LogLevel.Error:
+                    loggingLevel = "Error";
+                    break;
+                case LogLevel.Warning:
+                    loggingLevel = "Warning";
+                    break;
+                default:
+                    loggingLevel = "Debug";
+                    break;
+            }
+
+            string finalMessage = string.Format("{0} {1} {2} {3}",
+                                                DateTime.Now, ClassName, loggingLevel, message);
+            Console.WriteLine(finalMessage);
+        }
+    }
+}
