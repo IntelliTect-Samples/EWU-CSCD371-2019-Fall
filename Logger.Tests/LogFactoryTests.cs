8b91122858d51eb56d9134892ec5fff5eca14126
rowland17 <rowland17@up.edu>
2020-07-30 10:16:28 -0700

    Add test methods for behavior of each class.

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -6,6 +6,36 @@ namespace Logger.Tests
     [TestClass]
     public class LogFactoryTests
     {
-        
+        [TestMethod]
+        public void CreateFileLogger_PathSet_ReturnsFileLogger()
+        {
+            //Arrange
+            LogFactory factory = new LogFactory();
+
+            string path = Path.GetFullPath(Path.GetRandomFileName());
+
+            //Act
+            factory.ConfigureFileLogger(path);
+            BaseLogger logger = factory.CreateLogger("LogFactoryTests");
+
+
+            //Assert
+            Assert.IsTrue(logger is FileLogger);
+        }
+
+
+        [TestMethod]
+        public void CreateFileLogger_PathNotSet_ReturnsNull()
+        {
+            //Arrange
+            LogFactory factory = new LogFactory();
+
+            //Act
+            BaseLogger logger = factory.CreateLogger("LogFactoryTests");
+
+
+            //Assert
+            Assert.IsNull(logger);
+        }
     }
 }
