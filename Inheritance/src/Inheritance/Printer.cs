02f518ca0cc02a9f9225d903849136a991767677
rowland17 <rowland17@up.edu>
2020-08-03 16:01:35 -0700

    Part 1 of Assignment 3 with tests

--- Inheritance/src/Inheritance/Printer.cs
+++ Inheritance/src/Inheritance/Printer.cs
@@ -12,6 +12,7 @@ namespace Inheritance
             using (var sw = new StreamWriter(Console.OpenStandardOutput()))
             {
                 Printer.Print(new Food { Brand = "Frosted Mini Wheats", Upc = "123456789" }, sw);
+                Printer.Print(new Television { Manufacturer = "Toshiba", Size = "52\"" }, sw);
                 sw.Flush();
             }
         }
--- Inheritance/test/Inheritance.Tests/PrinterTests.cs
+++ Inheritance/test/Inheritance.Tests/PrinterTests.cs
@@ -7,15 +7,81 @@ using System.Text;
 namespace Inheritance.Tests
 {
     [TestClass]
-    public class PersonPrinterTests
+    public class PrinterTests
     {
         [TestMethod]
-        public void PersonGetsPrinted()
+        public void TelevisionGetsPrinted()
+        {
+            // Arrange
+            var television = new Television
+            {
+                Manufacturer = "Samsung",
+                Size = "42\""
+            };
+
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    // Act
+                    Printer.Print(television, writer);
+                    writer.Flush();
+
+                    stream.Position = 0;
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    // Assert
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var lineWritten = reader.ReadLine();
+                        Assert.AreEqual("Samsung - 42\"", lineWritten);
+                    }
+                }
+            }
+        }
+
+
+
+
+        [TestMethod]
+        public void FoodGetsPrinted()
+        {
+            // Arrange
+            var food = new Food
+            {
+                Upc = "123456789",
+                Brand = "Kirkland"
+            };
+
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    // Act
+                    Printer.Print(food, writer);
+                    writer.Flush();
+
+                    stream.Position = 0;
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    // Assert
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var lineWritten = reader.ReadLine();
+                        Assert.AreEqual("123456789 - Kirkland", lineWritten);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ItemGetsPrinted()
         {
             // Arrange
             var item = new TestItem { Name = "Test Item" };
 
-            using (var stream = new MemoryStream()) {
+            using (var stream = new MemoryStream())
+            {
                 using (var writer = new StreamWriter(stream))
                 {
                     // Act
@@ -36,7 +102,13 @@ namespace Inheritance.Tests
         }
     }
 
-    public class TestItem : Item {
+    public class TestItem : Item
+    {
         public string Name { get; set; }
+
+        public override string PrintInfo()
+        {
+            return Name;
+        }
     }
 }
