ac5d436e137433df8a38c71b171c407ecc6afcf5
rowland17 <rowland17@up.edu>
2020-08-19 14:25:29 -0700

    WIP Mailbox class

--- Mailbox/Mailbox.cs
+++ Mailbox/Mailbox.cs
@@ -1,4 +1,17 @@
 ﻿namespace Mailbox
 {
+    public class Mailbox
+    {
+        Size BoxSize { get; set; }
+
+        (int, int) Location { get; set; }
+
+        Person Owner { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Owner} - {Location} - {BoxSize}";
+        }
+    }
 
 }
