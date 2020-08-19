a4ecb6786ff8e7db46643b57cf57c6ac45f8a1bf
rowland17 <rowland17@up.edu>
2020-08-19 13:58:07 -0700

    implement Size Enum

--- Mailbox/Size.cs
+++ Mailbox/Size.cs
@@ -1,4 +1,17 @@
-﻿namespace Mailbox
-{
+﻿using System;
 
+namespace Mailbox
+{
+    [Flags]
+    public enum Size
+    {
+        Default = 0,
+        Small = 1 << 1,
+        Medium = 1 << 2,
+        Large = 1 << 3,
+        Premium = 1 << 4,
+        SmallPremium = Small | Premium,
+        MediumPremium = Medium | Premium,
+        LargePremium = Large | Premium
+    }
 }
