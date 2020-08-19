f1ab5dc070b13141e9c03530620b33803c73d009
rowland17 <rowland17@up.edu>
2020-08-19 14:19:48 -0700

    Add Nullable and implement Person.

--- Mailbox/Person.cs
+++ Mailbox/Person.cs
@@ -1,4 +1,42 @@
-﻿namespace Mailbox
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Mailbox
 {
+    public struct Person : IEquatable<Person>
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+        public bool Equals(Person other)
+        {
+            return FirstName == other.FirstName && LastName == other.LastName;
+        }
+
+        public override bool Equals([AllowNull] object obj)
+        {
+            return obj is Person && Equals((Person)obj);
+        }
+
+        public static bool operator ==(Person left, Person right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Person left, Person right)
+        {
+            return !(left == right);
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        public override int GetHashCode()
+        {
+            return (FirstName, LastName).GetHashCode();
+        }
+    }
 
 }
