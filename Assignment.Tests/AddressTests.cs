b5eda1c21cb854723a19bf1e7e36c00cc1a10877
rowland17 <rowland17@up.edu>
2020-09-16 11:08:12 -0700

    Add test files for each of the files.
    Create tests for all functions and fix bugs as I go.

---
+++ Assignment.Tests/AddressTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment.Tests
+{
+    [TestClass]
+    public class AddressTests
+    {
+        [DataTestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        [DataRow("", "City", "State", "Zip")]
+        [DataRow("Address", "", "State", "Zip")]
+        [DataRow("Address", "City", "", "Zip")]
+        [DataRow("Address", "City", "State", "")]
+        [DataRow(null, "City", "State", "zip")]
+        [DataRow("Address", null, "State", "zip")]
+        [DataRow("Address", "City", null, "zip")]
+        [DataRow("Address", "City", "State", null)]
+        public void Address_ConstructorNullOrEmptyParameters_ThrowsException(string address, string city, string state, string zip)
+        {
+            _ = new Address(address, city, state, zip);
+        }
+
+        [TestMethod]
+        public void Address_Constructor_AssignsValues()
+        {
+            string streetAddress = "Address";
+            string city = "City";
+            string state = "State";
+            string zip = "Zip";
+
+            Address address = new Address(streetAddress, city, state, zip);
+
+            Assert.AreEqual(streetAddress, address.StreetAddress);
+            Assert.AreEqual(city, address.City);
+            Assert.AreEqual(state, address.State);
+            Assert.AreEqual(zip, address.Zip);
+        }
+    }
+}
---
+++ Assignment.Tests/PersonTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment.Tests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [DataTestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        [DataRow("", "last", "email")]
+        [DataRow("first", "", "email")]
+        [DataRow("first", "last", "")]
+        [DataRow(null, "last", "email")]
+        [DataRow("first", null, "email")]
+        [DataRow("first", "last", null)]
+        public void Person_ConstructorNullOrWhitespace_ThrowsException(string first, string last, string email)
+        {
+            _ = new Person(first, last, new Address("street", "city", "state", "zip"), email);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Person_ConstructorNullAddress_ThrowsException()
+        {
+            _ = new Person("first", "last", null!, "email");
+        }
+
+        [TestMethod]
+        public void Person_Constructor_SetsValues()
+        {
+            string first = "first";
+            string last = "last";
+            string street = "street";
+            string city = "city";
+            string state = "state";
+            string zip = "zip";
+            string email = "email";
+
+            Person person = new Person(first, last, new Address(street, city, state, zip), email);
+
+            Assert.AreEqual(first, person.FirstName);
+            Assert.AreEqual(last, person.LastName);
+            Assert.AreEqual(street, person.Address.StreetAddress);
+            Assert.AreEqual(city, person.Address.City);
+            Assert.AreEqual(state, person.Address.State);
+            Assert.AreEqual(zip, person.Address.Zip);
+            Assert.AreEqual(email, person.EmailAddress);
+
+        }
+    }
+}
