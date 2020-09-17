7f0eabd775ad4d0de82a8adc937997eb7be3fb10
rowland17 <rowland17@up.edu>
2020-09-17 15:23:08 -0700

    WIP

---
+++ ShoppingList/ShoppingList/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace ShoppingList
+{
+    public class MainWindowViewModel : BaseNotifyPropertyChanged
+    {
+        private Item _SelectedItem = new Item();
+        public Item SelectedItem
+        {
+            get => _SelectedItem;
+            set => SetProperty(ref _SelectedItem, value);
+        }
+
+        public ObservableCollection<Item> ShoppingList { get; } = new ObservableCollection<Item>();
+
+        public ICommand AddListItemCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            AddListItemCommand = new Command(OnAddListItem);
+            //ShoppingList.Add(");
+        }
+
+        private void OnAddListItem()
+        {
+            ShoppingList.Add(new Item()
+            {
+                ItemName = $"List item",
+                ItemQuantity = ShoppingList.Count
+            });
+            OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(ShoppingList)));
+        }
+    }
+}
